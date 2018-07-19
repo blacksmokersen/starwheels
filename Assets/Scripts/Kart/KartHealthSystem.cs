031b476ce58b989a9dc4856123c8fb4bfee70ceb
Wydman <baptiste.int@gmail.com>
2018-07-19 15:05:45 +0200

    AntiSpamHitSecurity

--- Assets/Scripts/Kart/KartHealthSystem.cs
+++ Assets/Scripts/Kart/KartHealthSystem.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
-
+using System.Collections;
 
 namespace Kart
 {
     public class KartHealthSystem : MonoBehaviour
     {
         public int Health = 3;
+        public float SpamHitSecurity;
+        public float HitStopKartDuration;
+
 
         private KartEffects kartEffects;
         private KartOrientation KartOrientation;
+        private bool invicibility = false;
 
         private void Awake()
         {
@@ -18,14 +22,24 @@ namespace Kart
 
         public void HealtLoss()
         {
-            Health--;
-            KartOrientation.LooseHealth(2.5f);
-            kartEffects.HealthParticlesManagement(Health);
+            if (!invicibility)
+            {
+                Health--;
+                KartOrientation.LooseHealth(HitStopKartDuration);
+                kartEffects.HealthParticlesManagement(Health);
+                StartCoroutine(Invicibility(SpamHitSecurity));
+            }
             if(Health <= 0)
             {
 
                 GetComponentInParent<Rigidbody>().transform.position = new Vector3(-221, 3, 0);
             }
+        }
+        IEnumerator Invicibility(float invicibilityTimer)
+        {
+            invicibility = true;
+            yield return new WaitForSeconds(invicibilityTimer);
+            invicibility = false;
         }
     }
 }
