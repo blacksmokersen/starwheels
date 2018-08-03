76f4696dc809ef141a36f941be629e044c10076f
SimonT <simon.trebern@outlook.com>
2018-08-03 13:07:28 +0200

    Added HUD to kart prefab

--- Assets/Scripts/HUD/HUDUpdater.cs
+++ Assets/Scripts/HUD/HUDUpdater.cs
@@ -7,9 +7,11 @@ namespace HUD
 {
     public class HUDUpdater : MonoBehaviour
     {
+        /*
         public Text SpeedText;
         public Text TimeText;
         public Text FPSText;
+        */
         public Text ItemCountText;
         
         public RawImage ItemTexture;
@@ -26,8 +28,8 @@ namespace HUD
         {
             while (Application.isPlaying)
             {
-                TimeText.text = "Time : " + Time.time;
-                FPSText.text = "FPS : " + 1.0f / Time.deltaTime;
+                //TimeText.text = "Time : " + Time.time;
+                //FPSText.text = "FPS : " + 1.0f / Time.deltaTime;
                 yield return new WaitForSeconds(0.05f);
             }
         }
