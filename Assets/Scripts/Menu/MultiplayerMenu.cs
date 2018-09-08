7d78151eded1fdc090a7bf5e5d44d7b64b2fdbe6
wRadion <wradion@gmail.com>
2018-09-09 00:51:00 +0200

    194: wip multiplayer menu

---
+++ Assets/Scripts/Menu/MultiplayerMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiplayerMenu : MonoBehaviour {
+
+    [SerializeField] private Button createRoomButton;
+    [SerializeField] private Button refreshRoomButton;
+
+    // CORE
+
+    private void Awake()
+    {
+        createRoomButton.onClick.AddListener(() => FindObjectOfType<StringInput>().GetStringInput("Create a new room", CreateRoom));
+        refreshRoomButton.onClick.AddListener(RefreshRoom);
+    }
+
+    // PUBLIC
+
+    // PRIVATE
+    private void CreateRoom(string roomName)
+    {
+    }
+
+    private void RefreshRoom()
+    {
+    }
+}
