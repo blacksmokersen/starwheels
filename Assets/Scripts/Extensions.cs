0956efc4682abdc5cf1109b07cb85aa0d6a2da5a
SimonT <simon.trebern@outlook.com>
2018-09-10 00:14:24 +0200

    HotFix : Added CopyAudioSource

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using Photon.Realtime;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
+using System;
+using System.Reflection;
 
 namespace MyExtensions
 {
@@ -49,7 +51,7 @@ namespace MyExtensions
         public static Player PickRandomTeammate()
         {
             var teammates = GetTeammates();
-            var rand = Random.Range(0, teammates.Count - 1);
+            var rand = UnityEngine.Random.Range(0, teammates.Count - 1);
             return teammates[rand];
         }
     }
@@ -90,8 +92,65 @@ namespace MyExtensions
         public static GameObject PickRandomTeamKart()
         {
             var teamKart = GetTeamKarts();
-            var rand = Random.Range(0, teamKart.Count - 1);
+            var rand = UnityEngine.Random.Range(0, teamKart.Count - 1);
             return teamKart[rand];
+        }
+    }
+    #endregion
+
+    #region Components
+    public static class Components
+    {
+        public static T CopyComponent<T>(T original, GameObject destination) where T : Component
+        {
+            System.Type type = original.GetType();
+            var dst = destination.GetComponent(type) as T;
+            if (!dst) dst = destination.AddComponent(type) as T;
+            var fields = type.GetFields();
+            foreach (var field in fields)
+            {
+                if (field.IsStatic) continue;
+                field.SetValue(dst, field.GetValue(original));
+            }
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                if (!prop.CanWrite || !prop.CanWrite || prop.Name == "name") continue;
+                prop.SetValue(dst, prop.GetValue(original, null), null);
+            }
+            return dst as T;
+        }
+
+        public static void CopyAndPasteAudioSource(AudioSource original, GameObject destination)
+        {
+            destination.AddComponent<AudioSource>();
+            AudioSource audioSource = destination.GetComponent<AudioSource>();
+            audioSource.bypassEffects = original.bypassEffects;
+            audioSource.bypassListenerEffects = original.bypassListenerEffects;
+            audioSource.bypassReverbZones = original.bypassReverbZones;
+            audioSource.clip = original.clip;
+            audioSource.dopplerLevel = original.dopplerLevel;
+            audioSource.ignoreListenerPause = original.ignoreListenerPause;
+            audioSource.ignoreListenerVolume = original.ignoreListenerVolume;
+            audioSource.loop = original.loop;
+            audioSource.maxDistance = original.maxDistance;
+            audioSource.minDistance = original.minDistance;
+            audioSource.mute = original.mute;
+            audioSource.outputAudioMixerGroup = original.outputAudioMixerGroup;
+            audioSource.playOnAwake = original.playOnAwake;
+            audioSource.panStereo = original.panStereo;
+            audioSource.priority = original.priority;
+            audioSource.pitch = original.pitch;
+            audioSource.rolloffMode = original.rolloffMode;
+            audioSource.reverbZoneMix = original.reverbZoneMix;
+            audioSource.spatialBlend = original.spatialBlend;
+            audioSource.spatialize = original.spatialize;
+            audioSource.spatializePostEffects = original.spatializePostEffects;
+            audioSource.spread = original.spread;
+            audioSource.time = original.time;
+            audioSource.timeSamples = original.timeSamples;
+            audioSource.velocityUpdateMode = original.velocityUpdateMode;
+            audioSource.volume = original.volume;
         }
     }
     #endregion
@@ -103,8 +162,7 @@ namespace MyExtensions
         {
             GameObject oneShotObject = new GameObject("One shot sound from " + audioSource.name);
             oneShotObject.transform.position = audioSource.transform.position;
-            UnityEditorInternal.ComponentUtility.CopyComponent(audioSource);
-            UnityEditorInternal.ComponentUtility.PasteComponentAsNew(oneShotObject);
+            Components.CopyAndPasteAudioSource(audioSource, oneShotObject);
             var oneShotSource = oneShotObject.GetComponent<AudioSource>();
             oneShotSource.Play();
             MonoBehaviour.Destroy(oneShotObject, oneShotSource.clip.length);
