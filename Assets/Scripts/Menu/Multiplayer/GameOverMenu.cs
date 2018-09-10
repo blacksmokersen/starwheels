3e060b7e8cb6baf07d6a2453feb8991a45df0dd6
SimonT <simon.trebern@outlook.com>
2018-09-10 22:18:58 +0200

    137 : Created script for ReplayButton

---
+++ Assets/Scripts/Menu/Multiplayer/GameOverMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+namespace Menu
+{
+    public class GameOverMenu : MonoBehaviour
+    {
+        [SerializeField] private Button replayButton;
+
+        private void Awake()
+        {
+            replayButton.enabled = PhotonNetwork.IsMasterClient;
+        }
+    }
+}
