321bcbc08e37070631ca25f7eca31f73ccc5733e
SimonT <simon.trebern@outlook.com>
2018-10-01 21:12:30 +0200

    251 : Refacto multiplayerSettings

---
+++ Assets/Scripts/Refacto/NicknamePanel.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEditor;
+using TMPro;
+
+public class NicknamePanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro nameText;
+    [SerializeField] private SpriteRenderer frameRenderer;
+
+    public void SetName(string name)
+    {
+        nameText.text = name;
+    }
+
+    public void SetFrameRendererColor(Color color)
+    {
+        frameRenderer.color = color;
+    }
+
+    public void SetFrameRendererTeam(Team team)
+    {
+        frameRenderer.color = TeamsColors.GetTeamColor(team);
+    }
+
+    public void ShowPanel()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HidePanel()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+[CustomEditor(typeof(NicknamePanel))]
+public class NicknamePanelEditor : Editor
+{
+    Color frameColor;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        NicknamePanel nicknamePanel = (NicknamePanel)target;
+        if (GUILayout.Button("Show Panel"))
+            nicknamePanel.ShowPanel();
+        if (GUILayout.Button("Hide Panel"))
+            nicknamePanel.HidePanel();
+
+        frameColor = EditorGUILayout.ColorField("Change color", frameColor);
+        nicknamePanel.SetFrameRendererColor(frameColor);
+    }
+}
