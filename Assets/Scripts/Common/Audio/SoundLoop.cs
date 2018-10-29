87447cdde8f86de0df47607ced2c10925db39c0b
SimonT <simon.trebern@outlook.com>
2018-10-29 16:22:07 +0100

    misc: added crowd sound

---
+++ Assets/Scripts/Common/Audio/SoundLoop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+namespace Common.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundLoop : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<AudioSource>().loop = true;
+        }
+
+        public void Play()
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        public void Stop()
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+}
