e805d2124d5f7a3825d9c2904685d790074606ce
SimonT <simon.trebern@outlook.com>
2018-11-03 12:01:04 +0100

    273 : Debug Teleporter

---
+++ Assets/Scripts/Debug/DebugTeleporter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KBA.Debug
+{
+    public class DebugTeleporter : MonoBehaviour, IControllable
+    {
+        [Header("Teleport Settings")]
+        [SerializeField] private GameObject _teleportTarget;
+        [SerializeField] private Vector3 _teleportPosition;
+
+        // CORE
+
+        void Update()
+        {
+            MapInputs();
+        }
+
+        // PUBLIC
+
+        public void MapInputs()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha9))
+            {
+                _teleportTarget.transform.position = _teleportPosition;
+            }
+        }
+    }
+}
