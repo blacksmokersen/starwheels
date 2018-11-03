eab61c81f7d52f45e949fd07f4970909c24a05e9
SimonT <simon.trebern@outlook.com>
2018-11-03 17:00:40 +0100

    276 : Smoothing according the collision normal

---
+++ Assets/Scripts/KartPhysics/CollisionSmoother.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace KartPhysics
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CollisionSmoother : MonoBehaviour
+    {
+        [Header("Smooth Settings")]
+        [SerializeField] private bool _smoothCollisionWithGround = false;
+        [Tooltip("Value of 1f is really boucy, 0f means no smoothing.")]
+        [SerializeField] private float _smoothFactor = 1f;
+
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        // PRIVATE
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer(Constants.Layer.Ground))
+            {
+                _rb.constraints = RigidbodyConstraints.FreezeRotationY;
+
+                var collisionForce = collision.relativeVelocity.magnitude;
+                var collisionNormal = collision.contacts[0].normal;
+                if(!_smoothCollisionWithGround) collisionNormal.y = 0;
+
+                _rb.AddForce(collisionNormal * collisionForce * _smoothFactor, ForceMode.VelocityChange);
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (collision.gameObject.layer == LayerMask.NameToLayer(Constants.Layer.Ground))
+            {
+                _rb.constraints = RigidbodyConstraints.None;
+            }
+        }
+    }
+}
--- Assets/Scripts/KartPhysics/WheelSuspensions.cs
+++ Assets/Scripts/KartPhysics/WheelSuspensions.cs
@@ -1,50 +1,54 @@
 ﻿using UnityEngine;
 
-public class WheelSuspensions : MonoBehaviour
+namespace KartPhysics
 {
-    public float MaxExtensionDistance;
-    public float SuspensionStiffness;
-    public float ForceToApply;
-
-    [Header("Wheels transforms")]
-    [SerializeField] private Transform[] wheelsTransforms;
-
-    private Rigidbody _rb;
-
-    // CORE
-
-    private void Awake()
+    public class WheelSuspensions : MonoBehaviour
     {
-        _rb = GetComponentInParent<Rigidbody>();
-    }
+        [Header("Suspensions Settings")]
+        public float MaxExtensionDistance;
+        public float SuspensionStiffness;
+        public float ForceToApply;
 
-    private void FixedUpdate()
-    {
-        foreach (var wheelTransform in wheelsTransforms)
+        [Header("Wheels transforms")]
+        [SerializeField] private Transform[] wheelsTransforms;
+
+        private Rigidbody _rb;
+
+        // CORE
+
+        private void Awake()
         {
-            AdjustWheelPosition(wheelTransform);
+            _rb = GetComponentInParent<Rigidbody>();
         }
-    }
 
-    // PUBLIC
-
-    // PRIVATE
-
-    private void AdjustWheelPosition(Transform wheelTransform)
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(wheelTransform.position, Vector3.down, out hit, MaxExtensionDistance, 1 << LayerMask.NameToLayer(Constants.Layer.Ground)))
+        private void FixedUpdate()
         {
-            var distance = Mathf.Clamp(hit.distance, 0, MaxExtensionDistance);
-            var compressionRatio = - distance + MaxExtensionDistance;
-            _rb.AddForceAtPosition(ComputeForceToAdd(compressionRatio), wheelTransform.position, ForceMode.Acceleration);
+            foreach (var wheelTransform in wheelsTransforms)
+            {
+                AdjustWheelPosition(wheelTransform);
+            }
         }
-    }
 
-    private Vector3 ComputeForceToAdd(float compressionRatio)
-    {
-        var newSuspensionForce = transform.TransformVector(Vector3.up * compressionRatio * SuspensionStiffness);
-        return newSuspensionForce * ForceToApply;
+        // PUBLIC
+
+        // PRIVATE
+
+        private void AdjustWheelPosition(Transform wheelTransform)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(wheelTransform.position, Vector3.down, out hit, MaxExtensionDistance, 1 << LayerMask.NameToLayer(Constants.Layer.Ground)))
+            {
+                var distance = Mathf.Clamp(hit.distance, 0, MaxExtensionDistance);
+                var compressionRatio = -distance + MaxExtensionDistance;
+                _rb.AddForceAtPosition(ComputeForceToAdd(compressionRatio), wheelTransform.position, ForceMode.Acceleration);
+            }
+        }
+
+        private Vector3 ComputeForceToAdd(float compressionRatio)
+        {
+            var newSuspensionForce = transform.TransformVector(Vector3.up * compressionRatio * SuspensionStiffness);
+            return newSuspensionForce * ForceToApply;
+        }
     }
 }
