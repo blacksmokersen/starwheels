2fa6488f0b90c13d062c20c20a5ea6aba4255a82
Wydman <baptiste.int@gmail.com>
2018-11-07 16:12:27 +0100

    263 : Use Raycast for ExplosionTransform
    Former-commit-id: b5ff54bcfa26c4636ec84fb923e4bfce0b7506b0

--- Assets/Scripts/Items/ItemsScripts/IonBeam/IonBeamLaserBehaviour.cs
+++ Assets/Scripts/Items/ItemsScripts/IonBeam/IonBeamLaserBehaviour.cs
@@ -8,33 +8,36 @@ namespace Items
         //  [SerializeField] private ProjectileBehaviour projectileBehaviour;
 
         [SerializeField] private IonBeamLaserSettings ionBeamLaserSettings;
-        [SerializeField] private GameObject EffectiveAOE;
-        [SerializeField] private GameObject WarningPosition;
-        [SerializeField] private AudioSource ExplosionSource;
+        [SerializeField] private GameObject effectiveAOE;
+        [SerializeField] private GameObject warningPosition;
+        [SerializeField] private GameObject explosionParticles;
+        [SerializeField] private Transform raycastTransformOrigin;
+        [SerializeField] private AudioSource explosionSource;
 
         private Vector2 offset;
         private bool _damagePlayer = false;
+        private Coroutine _laserBehaviour;
 
         //CORE
 
         private void Awake()
         {
             ionBeamLaserSettings.onExplode = true;
-           // GameObject owner = GetComponent<Ownership>().gameObject;
+            // GameObject owner = GetComponent<Ownership>().gameObject;
             //float currentTimer = WarningPosition.transform.localScale.x;
         }
 
         private void Update()
         {
             offset = new Vector2(0, Time.deltaTime * ionBeamLaserSettings.SpeedOffset);
-            if (EffectiveAOE != null)
-                EffectiveAOE.GetComponent<Renderer>().material.mainTextureOffset += offset;
+            if (effectiveAOE != null)
+                effectiveAOE.GetComponent<Renderer>().material.mainTextureOffset += offset;
 
-            if (WarningPosition != null)
+            if (warningPosition != null)
             {
-                if (WarningPosition.transform.localScale.x >= ionBeamLaserSettings.MaxWarningScale)
+                if (warningPosition.transform.localScale.x >= ionBeamLaserSettings.MaxWarningScale)
                 {
-                    if (WarningPosition.transform.localScale.x >= ionBeamLaserSettings.MaxWarningScale / 2)
+                    if (warningPosition.transform.localScale.x >= ionBeamLaserSettings.MaxWarningScale / 2)
                     {
                         GrowingAoeWarning(ionBeamLaserSettings.GrowingWarningSpeed / 2);
                     }
@@ -56,17 +59,26 @@ namespace Items
         public void GrowingAoeWarning(float growSpeed)
         {
             float IncreaseSpeed = growSpeed * Time.deltaTime;
-            WarningPosition.transform.localScale += new Vector3(-IncreaseSpeed, 0, -IncreaseSpeed);
+            warningPosition.transform.localScale += new Vector3(-IncreaseSpeed, 0, -IncreaseSpeed);
         }
 
         public void Explosion()
         {
             if (ionBeamLaserSettings.onExplode)
             {
-                Destroy(EffectiveAOE);
-                Destroy(WarningPosition);
-                StartCoroutine(ParticuleEffect());
-                ionBeamLaserSettings.onExplode = false;
+                RaycastHit hit;
+                if (Physics.Raycast(raycastTransformOrigin.position, Vector3.down, out hit, 1000, 1 << LayerMask.NameToLayer(Constants.Layer.Ground)))
+                {
+                    explosionParticles.transform.position = hit.point;
+                    Destroy(effectiveAOE);
+                    Destroy(warningPosition);
+                    _laserBehaviour = StartCoroutine(ParticuleEffect());
+                    ionBeamLaserSettings.onExplode = false;
+                }
+                else
+                {
+                    BoltNetwork.Destroy(gameObject);
+                }
             }
         }
 
@@ -74,8 +86,8 @@ namespace Items
 
         IEnumerator ParticuleEffect()
         {
-            GetComponent<ParticleSystem>().Emit(3000);
-           // MyExtensions.AudioExtensions.PlayClipObjectAndDestroy(ExplosionSource);
+            explosionParticles.GetComponent<ParticleSystem>().Emit(3000);
+            // MyExtensions.AudioExtensions.PlayClipObjectAndDestroy(ExplosionSource);
             _damagePlayer = true;
             yield return new WaitForSeconds(0.1f);
             _damagePlayer = false;
