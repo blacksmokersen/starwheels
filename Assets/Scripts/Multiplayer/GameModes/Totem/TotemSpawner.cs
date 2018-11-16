58198a0094f6af455bd034319ee36e89e70a725b
Unknown <simon.trebern@outlook.com>
2018-11-16 16:14:27 +0100

    292 : Fixes on the totem

---
+++ Assets/Scripts/Multiplayer/GameModes/Totem/TotemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using Bolt;
+
+namespace GameModes.Totem
+{
+    public class TotemSpawner : GlobalEventListener
+    {
+        private bool _totemInstantiated = false;
+
+        public override void BoltStartDone()
+        {
+            InstantiateTotem();
+        }
+
+        public override void SceneLoadLocalDone(string map)
+        {
+            InstantiateTotem();
+        }
+
+        private void InstantiateTotem()
+        {
+            if(BoltNetwork.isConnected && BoltNetwork.isServer && !_totemInstantiated)
+            {
+                BoltNetwork.Instantiate(BoltPrefabs.Totem, transform.position, transform.rotation);
+                _totemInstantiated = true;
+            }
+        }
+    }
+}
