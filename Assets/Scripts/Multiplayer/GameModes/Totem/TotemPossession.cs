1e8fbb4ff033de9dfeca9243490ee44c571a11c4
Shashimee <simon.trebern@outlook.com>
2018-11-17 23:25:26 +0100

    281 : Redone all logic for the possession of the totem
    Former-commit-id: d8c12d4afe4a0543572eab042e71e79e14c1b46b

---
+++ Assets/Scripts/Multiplayer/GameModes/Totem/TotemPossession.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Multiplayer;
+using Bolt;
+
+namespace GameModes.Totem
+{
+    [RequireComponent(typeof(TotemPicker))]
+    public class TotemPossession : GlobalEventListener
+    {
+        public BoltEntity TotemEntity;
+
+        private TotemPicker _totemPicker;
+        private PlayerSettings _playerSettings;
+
+        private void Awake()
+        {
+            _totemPicker = GetComponent<TotemPicker>();
+            _playerSettings = Resources.Load<PlayerSettings>(Constants.Resources.PlayerSettings);
+        }
+
+        public override void OnEvent(TotemLost evnt)
+        {
+            if(evnt.OldOwnerID == _playerSettings.ConnectionID)
+            {
+                _totemPicker.UnsetTotem();
+                TotemEntity.ReleaseControl();
+            }
+            else if(evnt.NewOwnerID == _playerSettings.ConnectionID)
+            {
+                _totemPicker.SetTotem(TotemEntity.gameObject);
+                TotemEntity.TakeControl();
+            }
+        }
+    }
+}
