d6e3dde011adc91b127d62e0c536f1b187b05398
Wydman <baptiste.int@gmail.com>
2018-11-18 17:16:02 +0100

    293 : QutMatch
    Former-commit-id: d27bc7a4cf3042d3d5db8f29c199b7109b3cff18

---
+++ Assets/Scripts/Menu/InGameMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Bolt;
+
+public class InGameMenu : MonoBehaviour {
+
+    [SerializeField] private Button quitButton;
+    [SerializeField] private GameObject inGameMenuPanel;
+
+    private bool _menuEnabled = false;
+
+    private void Awake()
+    {
+        quitButton.onClick.AddListener(QuitMatch);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _menuEnabled = !_menuEnabled;
+            inGameMenuPanel.SetActive(_menuEnabled);
+        }
+    }
+
+    private void QuitMatch()
+    {
+        Debug.Log("Quit Match");
+        BoltLauncher.Shutdown();
+        BoltNetwork.LoadScene("Menu");
+    }
+
+
+
+
+
+}
