c17c034ac337139e7d3c7dfd34485244b21e4016
Unknown <nicolas.monteil@devinci.fr>
2018-11-23 13:29:56 +0000

    288-Tp Back rework V1
    Former-commit-id: 95aee0cc7af2a562b0b76c9094223d3603325b7c

---
+++ Assets/TP Back/LookAtYAxis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtYAxis : MonoBehaviour {
+
+    public GameObject target;
+
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+            transform.LookAt(targetPosition);
+		
+	}
+}
