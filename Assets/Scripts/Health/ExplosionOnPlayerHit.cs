7a879fafa396defa7ee388dcaf5f2ae4dd93d0d8
Wydman <baptiste.int@gmail.com>
2018-11-24 16:37:24 +0100

    296 : First Push OnEvent PlayerHit
    Former-commit-id: b1c4de612eebafa5d50a85efb1be75206727d378

---
+++ Assets/Scripts/Health/ExplosionOnPlayerHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class ExplosionOnPlayerHit : GlobalEventListener
+{
+    public override void OnEvent(PlayerHit playerLaunchItem)
+    {
+        Debug.Log("TETETETETET");
+        gameObject.SetActive(true);
+    }
+}
