060873e6e548a62318b21888bd126b53f233478c
Shashimee <simon.trebern@outlook.com>
2018-11-26 19:49:20 +0100

    302 : Integrated drift effects

---
+++ Assets/Scripts/Drift/DriftTrailController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drift
+{
+    public class DriftTrailController : MonoBehaviour
+    {
+        [SerializeField] private List<TrailRenderer> _groundTrails;
+
+        private void Start()
+        {
+            SetTrailsActive(false);
+        }
+
+        public void SetTrailsActive(bool b)
+        {
+            foreach(var trail in _groundTrails)
+            {
+                trail.emitting = b;
+            }
+        }
+    }
+}
