54bd7b281402fd31e83349d5399505fb07f7ce36
Wydman <baptiste.int@gmail.com>
2018-11-30 17:15:48 +0100

    309 : Activation and Link Abilities
    Former-commit-id: 35b6ad8d7e23ac8929aab84b9dcb3fc335f327df

--- Assets/Scripts/Abilities/Cloak/CloakAbility.cs
+++ Assets/Scripts/Abilities/Cloak/CloakAbility.cs
@@ -1,12 +1,53 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CloakAbility : MonoBehaviour
-{
-    [SerializeField] private CloakSettings _cloakSettings;
-    [SerializeField] private GameObject _cloakEffect;
+namespace Abilities {
+    public class CloakAbility : AbilitiesBehaviour, IControllable
+    {
+        [SerializeField] private CloakSettings _cloakSettings;
+        [SerializeField] private GameObject _cloakEffect;
+        [SerializeField] private GameObject _kartMeshes;
+        [SerializeField] private Animator _animator;
+
+        private bool _canUseAbility = true;
 
 
+        private void Awake()
+        {
 
+        }
+
+        public override void SimulateController()
+        {
+            if (gameObject.activeInHierarchy)
+                MapInputs();
+        }
+
+        public void MapInputs()
+        {
+            if (Input.GetButtonDown(Constants.Input.UseAbility))
+            {
+                Use();
+            }
+        }
+
+
+        private void Use()
+        {
+            if (_canUseAbility)
+            {
+                Debug.Log("used cloak");
+              //  _cloakEffect.SetActive(true);
+                _animator.SetTrigger("ActivateCloakEffect");
+                StartCoroutine(AbilityCooldown(_cloakSettings.CooldownDuration));
+            }
+        }
+
+        private IEnumerator AbilityCooldown(float Duration)
+        {
+            _canUseAbility = false;
+            yield return new WaitForSeconds(Duration);
+            _canUseAbility = true;
+        }
+    }
 }
