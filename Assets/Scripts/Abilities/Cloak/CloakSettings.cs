d6198968f9484a3bf8cc234a9e6330a4aa3ef97f
Wydman <baptiste.int@gmail.com>
2018-11-30 16:36:04 +0100

    309 : Package and Cloak Structure
    Former-commit-id: f096eec40fcae7482be23399d68a79355be2c914

---
+++ Assets/Scripts/Abilities/Cloak/CloakSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities Settings/Cloak")]
+public class CloakSettings : ScriptableObject
+{
+    [Header("Duration")]
+    public float CloakDuration;
+
+    [Header("Cooldown")]
+    public float CooldownDuration;
+}
