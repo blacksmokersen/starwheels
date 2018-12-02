aca7e309afd9ac42633e42b484081e8af44a98c5
Wydman <baptiste.int@gmail.com>
2018-12-02 16:34:48 +0100

    313 : New System - Need Testing
    Former-commit-id: 380fb091fef593bc14aa9965a730f026338c9b3e

--- Assets/Scripts/Items/ItemsScripts/KartCollisionTrigger.cs
+++ Assets/Scripts/Items/ItemsScripts/KartCollisionTrigger.cs
@@ -7,6 +7,9 @@ namespace Items
     {
         [SerializeField] private ItemCollision _itemCollision;
 
+        private bool _imunity;
+        private GameObject _imunityTarget;
+
         private void OnTriggerEnter(Collider other)
         {
             if (BoltNetwork.isServer && entity.isAttached)
@@ -19,7 +22,26 @@ namespace Items
 
                     if (itemEntity.isAttached && itemEntity.TryFindState<IItemState>(out itemState)) // It is a concrete item
                     {
-                        if (itemState.Team != state.Team || itemState.OwnerID == state.OwnerID) // It's a hit
+                        if (itemState.OwnerID == state.OwnerID)
+                        {
+
+                            if (!_imunity && _imunityTarget != other.gameObject)
+                            {
+                                _imunityTarget = other.gameObject;
+                                _imunity = true;
+                            }
+
+                            if(/*other.gameObject == _imunityTarget &&*/ !_imunity)
+                            {
+                                PlayerHit playerHitEvent = PlayerHit.Create();
+                                playerHitEvent.PlayerEntity = entity;
+                                playerHitEvent.Send();
+                            }
+
+
+
+                        }
+                        else if (itemState.Team != state.Team) // It's a hit
                         {
                             PlayerHit playerHitEvent = PlayerHit.Create();
                             playerHitEvent.PlayerEntity = entity;
@@ -27,11 +49,36 @@ namespace Items
                         }
 
                         var otherItemCollision = other.GetComponent<ItemCollisionTrigger>().ItemCollision;
-                        if (otherItemCollision.ShouldBeDestroyed(_itemCollision)) // The item should be destroyed
+                        if (otherItemCollision.ShouldBeDestroyed(_itemCollision) && !_imunity) // The item should be destroyed
                         {
                             DestroyEntity destroyEntityEvent = DestroyEntity.Create();
                             destroyEntityEvent.Entity = other.GetComponentInParent<BoltEntity>();
                             destroyEntityEvent.Send();
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag(Constants.Tag.CollisionHitBox) &&
+                   other.GetComponent<ItemCollisionTrigger>().ItemCollision.ItemName != ItemCollisionName.Totem) // It is an item collision (except totem)
+            {
+                BoltEntity itemEntity = other.GetComponentInParent<BoltEntity>();
+                IItemState itemState;
+
+                if (itemEntity.isAttached && itemEntity.TryFindState<IItemState>(out itemState)) // It is a concrete item
+                {
+                    if (itemState.Team != state.Team || itemState.OwnerID == state.OwnerID) // It's a hit
+                    {
+                        if(itemState.OwnerID == state.OwnerID)
+                        {
+                            if (other.gameObject == _imunityTarget)
+                            {
+                                _imunity = false;
+                            }
                         }
                     }
                 }
