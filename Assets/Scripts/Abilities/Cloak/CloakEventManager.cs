4f821af9f0fbdf6644b80f1a897a176b0f32e599
Wydman <baptiste.int@gmail.com>
2018-12-03 16:15:04 +0100

    309 : Reintegration Cloak Scripts
    Former-commit-id: 8695a3bf6dd6f759177cfc3d5497fc1bbe2609bb

---
+++ Assets/Scripts/Abilities/Cloak/CloakEventManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+namespace Abilities
+{
+    public class CloakEventManager : GlobalEventListener
+    {
+        [SerializeField] CloakAbility cloakAbility;
+        /*
+        public override void OnEvent(CloakAbilityEvent evnt)
+        {
+            var entity = GetComponentInParent<BoltEntity>();
+            Debug.LogError("onEventCloak");
+            if (entity == evnt.BoltEntity)
+            {
+                Debug.LogError("onEventCloakINIF");
+                cloakAbility.CanUseAbility = evnt.CanUseAbility;
+             //   cloakAbility.Entity = evnt.BoltEntity;
+                cloakAbility.Use();
+            }
+        }
+        */
+    }
+}
