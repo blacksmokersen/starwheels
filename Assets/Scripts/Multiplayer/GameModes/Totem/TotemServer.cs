71551312b1c92263ef3d9582610985d38068a50c
Shashimee <simon.trebern@outlook.com>
2018-12-03 15:43:38 +0100

    312 : Working score for Totem
    Former-commit-id: bd7ae0b52aa34754af1ae81daddde88256737f26

---
+++ Assets/Scripts/Multiplayer/GameModes/Totem/TotemServer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Multiplayer.Teams;
+
+namespace GameModes.Totem
+{
+    [BoltGlobalBehaviour(BoltNetworkModes.Server, BoltScenes.CarapaceDebug, BoltScenes.CarapaceOvale, BoltScenes.CarapaceSquare)]
+    public class TotemServer : GameModeBase
+    {
+        // BOLT
+
+        public override void OnEvent(TotemWallHit evnt)
+        {
+            Debug.Log("TotemWallHit event received.");
+            var team = TeamsColors.GetTeamFromColor(evnt.Team);
+            IncreaseScore(team);
+        }
+    }
+}
