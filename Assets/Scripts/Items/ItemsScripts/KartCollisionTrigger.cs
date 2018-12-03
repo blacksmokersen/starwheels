4131289c31794b79f1fd1bc3e824990e5f50f878
Wydman <baptiste.int@gmail.com>
2018-12-03 20:02:14 +0100

    313 : Rework Imunity
    Former-commit-id: dd0da44b7376d5c8c3913e64ba22f2a16167b7ca

--- Assets/Scripts/Items/ItemsScripts/KartCollisionTrigger.cs
+++ Assets/Scripts/Items/ItemsScripts/KartCollisionTrigger.cs
@@ -22,19 +22,19 @@ namespace Items
 
                     if (itemEntity.isAttached && itemEntity.TryFindState<IItemState>(out itemState)) // It is a concrete item
                     {
-                        if (itemState.OwnerID == state.OwnerID)
+                        if (itemState.OwnerID == state.OwnerID || itemState.OwnerID == 0)
                         {
 
                             if (!_imunity && _imunityTarget != other.gameObject)
                             {
                                 _imunityTarget = other.gameObject;
                                 _imunity = true;
-                                Debug.LogError("imunity true");
+                             //   Debug.LogError("imunity true");
                             }
 
                             if(/*other.gameObject == _imunityTarget &&*/ !_imunity)
                             {
-                                Debug.LogError("kill");
+                             //   Debug.LogError("kill");
                                 PlayerHit playerHitEvent = PlayerHit.Create();
                                 playerHitEvent.PlayerEntity = entity;
                                 playerHitEvent.Send();
@@ -77,9 +77,9 @@ namespace Items
                     {
                         if(itemState.OwnerID == state.OwnerID)
                         {
-                            if (other.gameObject == _imunityTarget)
+                            if (other.gameObject == _imunityTarget || itemState.OwnerID == 0 || itemState.OwnerID == 2)
                             {
-                                Debug.LogError("imunity false");
+                              //  Debug.LogError("imunity false");
                                 _imunity = false;
                             }
                         }
