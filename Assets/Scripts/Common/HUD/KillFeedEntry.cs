dc9ab3f8be53b5966e6242dca4666a8c19964f87
Shashimee <simon.trebern@outlook.com>
2018-12-06 12:59:27 +0100

    244 : Added color to kill entry

---
+++ Assets/Scripts/Common/HUD/KillFeedEntry.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Common.HUD
+{
+    public class KillFeedEntry : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _killerName;
+
+        public void SetKillerName(string name)
+        {
+
+        }
+
+        public void SetItemIcon(string name)
+        {
+
+        }
+
+        public void SetVictimName(string name)
+        {
+
+        }
+    }
+}
