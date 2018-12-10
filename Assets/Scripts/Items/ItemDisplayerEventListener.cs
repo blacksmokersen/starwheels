2d9078735a8eeac46bc2e7eba0042f02a085f13a
Shashimee <simon.trebern@outlook.com>
2018-12-10 14:08:51 +0100

    311 : Removed Hide event, reworked logic
    Former-commit-id: b64352e4999d528939def6d4f0d3f64d1c941d2a

---
+++ Assets/Scripts/Items/ItemDisplayerEventListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Bolt;
+
+namespace Items
+{
+    [RequireComponent(typeof(ItemDisplayer))]
+    public class ItemDisplayerEventListener : GlobalEventListener
+    {
+        private ItemDisplayer _itemDisplayer;
+
+        // CORE
+
+        private void Awake()
+        {
+            _itemDisplayer = GetComponent<ItemDisplayer>();
+        }
+
+        // BOLT
+
+        public override void OnEvent(ShowKartDisplayItem evnt)
+        {
+            var entity = GetComponentInParent<BoltEntity>();
+
+            Debug.Log("Received display event : " + evnt.ItemCount);
+
+            if (entity == evnt.Entity)
+            {
+                var direction = evnt.DisplayForward ? Direction.Forward : Direction.Backward;
+                _itemDisplayer.DisplayItem(evnt.ItemName, evnt.ItemCount, direction);
+            }
+        }
+    }
+}
