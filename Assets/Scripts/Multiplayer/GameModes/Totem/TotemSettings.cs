16556dcc04fe29c78be59399340b7590533b1634
Shashimee <simon.trebern@outlook.com>
2018-12-12 16:20:07 +0100

    333 : Reworked prefab & added TotemSettings

---
+++ Assets/Scripts/Multiplayer/GameModes/Totem/TotemSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace GameModes.Totem
+{
+    [CreateAssetMenu(menuName = "Totem/Totem Settings")]
+    public class TotemSettings : ScriptableObject
+    {
+        [Header("Slowdown Settings")]
+        public float SecondsBeforeSlowdown = 1f;
+        public float SlowdownFactor = 0.98f;
+        public float StopMagnitudeThreshold = 0.1f;
+
+        [Header("Speed")]
+        public float Speed;
+
+        [Header("Pick Up")]
+        public float Radius;
+        public float SecondsBeforeCanBePickedAgain;
+
+        [Header("Owner")]
+        public bool CanUseAbilityWhenOwner;
+        public float SecondsBeforeCanUseAbilityAgain;
+        public bool CanUseItemsWhenOwner;
+        public float SecondsBeforeCanUseItemsAgain;
+    }
+}
