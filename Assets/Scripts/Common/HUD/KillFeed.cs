31a1064935b03290bff11e9be7cca8a564c46a88
Shashimee <simon.trebern@outlook.com>
2018-12-13 13:59:22 +0100

    244 : Started working on scripts

--- Assets/Scripts/Common/HUD/KillFeed.cs
+++ Assets/Scripts/Common/HUD/KillFeed.cs
@@ -1,11 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Bolt;
 
 namespace Common.HUD
 {
-    public class KillFeed : MonoBehaviour
+    public class KillFeed : GlobalEventListener
     {
+        [Header("Layout")]
+        [SerializeField] private VerticalLayoutGroup _killFeedLayout;
 
+        [Header("Entry")]
+        [SerializeField] private GameObject _entryPrefab;
+
+        // BOLT
+
+        public override void OnEvent(KartDestroyed evnt)
+        {
+            CreateEntry("Hello", "world", "bitch");
+        }
+
+        // PRIVATE
+
+        private void CreateEntry(string killerName, string item, string victimName)
+        {
+            var entry = Instantiate(_entryPrefab).GetComponent<KillFeedEntry>();
+            entry.SetKillerName(killerName);
+            entry.SetVictimName(victimName);
+            _killFeedLayout.transform.SetAsFirstSibling();
+        }
     }
 }
