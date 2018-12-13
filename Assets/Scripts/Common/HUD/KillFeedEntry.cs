cda95a95e6635676b1c069e2bf820b3fc99a4446
Shashimee <simon.trebern@outlook.com>
2018-12-13 13:59:22 +0100

    244 : Started working on scripts
    Former-commit-id: 31a1064935b03290bff11e9be7cca8a564c46a88

--- Assets/Scripts/Common/HUD/KillFeedEntry.cs
+++ Assets/Scripts/Common/HUD/KillFeedEntry.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace Common.HUD
@@ -6,20 +7,22 @@ namespace Common.HUD
     public class KillFeedEntry : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _killerName;
+        [SerializeField] private Image _itemLogo;
+        [SerializeField] private TextMeshProUGUI _victimName;
 
         public void SetKillerName(string name)
         {
-
+            _killerName.text = name;
         }
 
-        public void SetItemIcon(string name)
+        public void SetItemIcon(Image itemLogo)
         {
-
+            _itemLogo = itemLogo;
         }
 
         public void SetVictimName(string name)
         {
-
+            _victimName.text = name;
         }
     }
 }
