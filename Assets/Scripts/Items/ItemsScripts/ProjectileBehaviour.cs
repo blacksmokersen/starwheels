3d1ccc7677a6b9563d43fb3311f2ee7c32801195
Shashimee <simon.trebern@outlook.com>
2018-12-17 11:16:00 +0100

    247 : Changed ItemActivation & use OnCollision event for audio
    Former-commit-id: 40ea8965bfbb451be13c677b2a8d0eac607e32c3

--- Assets/Scripts/Items/ItemsScripts/ProjectileBehaviour.cs
+++ Assets/Scripts/Items/ItemsScripts/ProjectileBehaviour.cs
@@ -46,23 +46,23 @@ namespace Items
         }
 
         #region Audio
-        protected void PlayLaunchSound()
+        public void PlayLaunchSound()
         {
             LaunchSource.Play();
         }
 
-        protected void PlayFlySound()
+        public void PlayFlySound()
         {
             FlySource.loop = true;
             FlySource.Play();
         }
 
-        protected void PlayPlayerHitSound()
+        public void PlayPlayerHitSound()
         {
             MyExtensions.AudioExtensions.PlayClipObjectAndDestroy(PlayerHitSource);
         }
 
-        protected void PlayCollisionSound()
+        public void PlayCollisionSound()
         {
             MyExtensions.AudioExtensions.PlayClipObjectAndDestroy(CollisionSource);
         }
