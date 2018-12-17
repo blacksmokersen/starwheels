fda096018f87e57315c208fec76be4cdaf6e1648
Shashimee <simon.trebern@outlook.com>
2018-12-17 16:10:50 +0100

    develop [fix] : Fixed RocketLock behaviour

--- Assets/Scripts/Common/HUD/KillFeed.cs
+++ Assets/Scripts/Common/HUD/KillFeed.cs
@@ -7,18 +7,13 @@ namespace Common.HUD
     [RequireComponent(typeof(VerticalLayoutGroup))]
     public class KillFeed : GlobalEventListener
     {
-        [Header("Entry Settings")]
-        [SerializeField] private float _entryLifespan = 5f;
-
         [Header("Entry")]
         [SerializeField] private GameObject _entryPrefab;
 
-        private VerticalLayoutGroup _killFeedLayout;
         private Items.ItemListData _itemListData;
 
         private void Awake()
         {
-            _killFeedLayout = GetComponent<VerticalLayoutGroup>();
             _itemListData = Resources.Load<Items.ItemListData>(Constants.Resources.ItemListData);
         }
 
