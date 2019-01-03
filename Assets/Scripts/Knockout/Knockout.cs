947e012084523e978ae32f383ce1176fc61e7537
Shashimee <simon.trebern@outlook.com>
2019-01-03 16:25:36 +0100

    362 : Created prefabs & changed namespace
    Former-commit-id: dc106d900aca632fcb223b4e622970e0ac4f7224

--- Assets/Scripts/Knockout/Knockout.cs
+++ Assets/Scripts/Knockout/Knockout.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace Multiplayer
+namespace Knockout
 {
     public class Knockout : MonoBehaviour
     {
---
+++ Assets/Scripts/Knockout/KnockoutAnimations.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Knockout
+{
+    public class KnockoutAnimations : MonoBehaviour
+    {
+        [Header("Animations")]
+        [SerializeField] private Animator _animator;
+
+        public void PlayKnockoutAnimation()
+        {
+            _animator.SetTrigger(Constants.Animations.Knockout);
+        }
+    }
+}
