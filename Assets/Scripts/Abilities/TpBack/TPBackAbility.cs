dfae7a19292f3607b0ed62467cd958ffbc4f08dc
Shashimee <simon.trebern@outlook.com>
2019-01-05 20:25:28 +0100

    362 : Smoothed knockout & fixed errors

--- Assets/Scripts/Abilities/TpBack/TPBackAbility.cs
+++ Assets/Scripts/Abilities/TpBack/TPBackAbility.cs
@@ -29,7 +29,7 @@ namespace Abilities
 
         private void Update()
         {
-            if (entity.isControllerOrOwner && gameObject.activeInHierarchy)
+            if (entity.isAttached && entity.isControllerOrOwner && gameObject.activeInHierarchy)
             {
                 MapInputs();
             }
