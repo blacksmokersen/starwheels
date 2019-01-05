6b776c3c28adc8e68bde4904a1a1867d5a3133a8
Shashimee <simon.trebern@outlook.com>
2019-01-05 17:25:20 +0100

    362 : Reworked all kart destroy logic & created RespawnRequest event
    Former-commit-id: 93f60dc8e95c45b19255609be2d10d86549c578e

---
+++ Assets/Scripts/KartDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Bolt;
+using Multiplayer;
+
+public class KartDestroyer : GlobalEventListener
+{
+    [Header("Kart Root")]
+    [SerializeField] private BoltEntity _rootEntity;
+
+    [Header("Player Settings")]
+    [SerializeField] private PlayerSettings _settings;
+
+    public override void OnEvent(KartDestroyed evnt)
+    {
+        if (evnt.ConnectionID == _settings.ConnectionID)
+        {
+            BoltNetwork.Destroy(_rootEntity);
+        }
+    }
+}
