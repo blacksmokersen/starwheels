8079a10b61ad005284b7eda7cf2a661b7747be72
Shashimee <simon.trebern@outlook.com>
2019-01-07 11:36:01 +0100

    362 : Fixed explosion on destroy

--- Assets/Scripts/Health/ExplosionOnPlayerHit.cs
+++ Assets/Scripts/Health/ExplosionOnPlayerHit.cs
@@ -7,11 +7,16 @@ namespace Health
     {
         [SerializeField] private GameObject _explosionEffect;
 
-        public void HideKartMeshAndExplode()
+        public void InvokeExplosion()
         {
             _explosionEffect.SetActive(true);
             _explosionEffect.GetComponent<SelfDestroyer>().StartCountdown();
             _explosionEffect.transform.SetParent(null);
+        }
+
+        private void OnDestroy()
+        {
+            InvokeExplosion();
         }
     }
 }
--- Assets/Scripts/KartDestroyer.cs
+++ Assets/Scripts/KartDestroyer.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using Bolt;
 using Multiplayer;
+using System.Collections;
 
 public class KartDestroyer : EntityBehaviour
 {
@@ -10,11 +11,26 @@ public class KartDestroyer : EntityBehaviour
     [Header("Player Settings")]
     [SerializeField] private PlayerSettings _settings;
 
+    // PUBLIC
+
     public void DestroyKart()
     {
         if (entity.isOwner)
         {
             BoltNetwork.Destroy(_rootEntity);
         }
+    }
+
+    public void DestroyKartAfterXSeconds(float x)
+    {
+        StartCoroutine(DestroyKartAfterXSecondsRoutine(x));
+    }
+
+    // PRIVATE
+
+    private IEnumerator DestroyKartAfterXSecondsRoutine(float x)
+    {
+        yield return new WaitForSeconds(x);
+        DestroyKart();
     }
 }
