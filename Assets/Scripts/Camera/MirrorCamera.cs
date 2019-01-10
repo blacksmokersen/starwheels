9b31e59ccbbd6d1bd42816ff38cec5be6c128e7f
Shashimee <simon.trebern@outlook.com>
2019-01-10 18:50:06 +0100

    2018.3 : Added new HUD for killfeed & flipped mirror

---
+++ Assets/Scripts/Camera/MirrorCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace CameraUtils
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Camera))]
+    public class MirrorCamera : MonoBehaviour
+    {
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void OnPreCull()
+        {
+            Debug.Log("OnPreCull");
+            _camera.ResetWorldToCameraMatrix();
+            _camera.ResetProjectionMatrix();
+            _camera.projectionMatrix *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        }
+
+        private void OnPreRender()
+        {
+            Debug.Log("OnPreRender");
+            GL.invertCulling = true;
+        }
+
+        private void OnPostRender()
+        {
+            Debug.Log("OnPostRender");
+            GL.invertCulling = false;
+        }
+    }
+}
