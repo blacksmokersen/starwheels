00b23a2c54c839fff1ed7cce43069446192aee91
Shashimee <simon.trebern@outlook.com>
2019-01-10 12:01:56 +0100

    334 : Working initializing phase

--- Assets/Scripts/Items/ItemHUD.cs
+++ Assets/Scripts/Items/ItemHUD.cs
@@ -6,9 +6,13 @@ namespace Items
 {
     public class ItemHUD : MonoBehaviour
     {
-        [Header("Item")]
+        [Header("HUD")]
         [SerializeField] private TextMeshProUGUI _itemCountText;
         [SerializeField] private Image _itemTexture;
+
+        [Header("Initializing State")]
+        [SerializeField] private Image _initializingSprite;
+        [SerializeField] private Image _loadingGauge;
 
         // CORE
 
@@ -32,6 +36,15 @@ namespace Items
                 kartInventory.OnItemUse.AddListener(UpdateItem);
                 kartInventory.OnItemCountChange.AddListener(UpdateItemCount);
             }
+
+            var kartLottery = kartRoot.GetComponentInChildren<Lottery>();
+            if (kartLottery)
+            {
+                kartLottery.OnLotteryStart.AddListener(ShowInitializingState);
+                kartLottery.OnLotteryUpdate.AddListener(UpdateLoadingGauge);
+                kartLottery.OnLotteryStop.AddListener(() => UpdateLoadingGauge(0f));
+                kartLottery.OnLotteryStop.AddListener(HideInitializingState);
+            }
         }
 
         public void UpdateItem(Item item)
@@ -51,6 +64,23 @@ namespace Items
         public void UpdateItemCount(int count)
         {
             _itemCountText.text = "" + count;
+        }
+
+        // PRIVATE
+
+        private void ShowInitializingState()
+        {
+            _initializingSprite.enabled = true;
+        }
+
+        private void HideInitializingState()
+        {
+            _initializingSprite.enabled = false;
+        }
+
+        private void UpdateLoadingGauge(float percentage)
+        {
+            _loadingGauge.fillAmount = percentage;
         }
     }
 }
