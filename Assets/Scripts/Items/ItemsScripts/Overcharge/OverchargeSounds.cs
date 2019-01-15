ad7faff90cf6a10ff97ad67da77e7aa1bd1bfccd
Shashimee <simon.trebern@outlook.com>
2019-01-15 15:09:45 +0100

    345 : Working prototype
    Former-commit-id: dc298a0827d60bb53d7e11aad5f316b72ebfa1a8

---
+++ Assets/Scripts/Items/ItemsScripts/Overcharge/OverchargeSounds.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using MyExtensions;
+
+namespace Items
+{
+    public class OverchargeSounds : MonoBehaviour
+    {
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource _activationAudioSource;
+        [SerializeField] private AudioSource _deactivationAudioSource;
+
+        public void PlayActivationSound()
+        {
+            _activationAudioSource.Play();
+        }
+
+        public void PlayDeactivationSound()
+        {
+            AudioExtensions.PlayClipObjectAndDestroy(_deactivationAudioSource);
+        }
+    }
+}
