62a4a3de537a7e8bbabcc3ade462001d11766012
Shashimee <simon.trebern@outlook.com>
2019-01-15 15:25:05 +0100

    346 : Created script

---
+++ Assets/Scripts/Items/ItemMerger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items
+{
+    public class ItemMerger : MonoBehaviour, IControllable
+    {
+        public void MapInputs()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
