9bc56e13dc50f2d99158d3083f9cfccf92170a46
Shashimee <simon.trebern@outlook.com>
2019-01-20 15:32:54 +0100

    develop : Added size increase on guile

---
+++ Assets/Scripts/SizeIncreaseSettings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Common Settings/Size Increase")]
+public class SizeIncreaseSettings : ScriptableObject
+{
+    public float SecondsBeforeFullSize;
+    public Vector3 TargetSize;
+    public Vector3 StartSize;
+}
