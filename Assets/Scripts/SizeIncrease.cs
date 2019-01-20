62fe629ef62aff1774f6ee8f073f9e0b39e9ae9e
Shashimee <simon.trebern@outlook.com>
2019-01-20 15:32:54 +0100

    develop : Added size increase on guile
    Former-commit-id: 9bc56e13dc50f2d99158d3083f9cfccf92170a46

---
+++ Assets/Scripts/SizeIncrease.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/*
+ * May have to synchronize the size with a throwable state
+ *
+ */
+public class SizeIncrease : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private SizeIncreaseSettings settings;
+
+    private float _timer = 0f;
+    private Vector3 _increaseRatio;
+
+    // CORE
+
+    private void Awake()
+    {
+        _increaseRatio = settings.TargetSize - settings.StartSize;
+    }
+
+    private void Update()
+    {
+        if (_timer < settings.SecondsBeforeFullSize)
+        {
+            _timer += Time.deltaTime;
+            IncreaseSize();
+        }
+    }
+
+    // PRIVATE
+
+    private void IncreaseSize()
+    {
+        var currentScale = transform.localScale;
+        currentScale.x = settings.StartSize.x + _timer / settings.SecondsBeforeFullSize * _increaseRatio.x;
+        currentScale.y = settings.StartSize.y + _timer / settings.SecondsBeforeFullSize * _increaseRatio.y;
+        currentScale.z = settings.StartSize.z + _timer / settings.SecondsBeforeFullSize * _increaseRatio.z;
+        transform.localScale = currentScale;
+    }
+}
