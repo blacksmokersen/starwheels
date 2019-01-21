be8c614278e3f54a09c72bd3ae7ac4d0151ea268
Shashimee <simon.trebern@outlook.com>
2019-01-21 16:59:51 +0100

    404 : Created SO for TeamColors

---
+++ Assets/Scripts/Multiplayer/GameModes/GameModeSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameModes
+{
+    [CreateAssetMenu(menuName ="GameMode Settings")]
+    public class GameModeSettings : MonoBehaviour
+    {
+        public GameMode CurrentGameMode;
+    }
+}
---
+++ Assets/Scripts/Multiplayer/Teams/TeamColorSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Multiplayer.Teams
+{
+    public class TeamColorSettings : MonoBehaviour
+    {
+        [Header("Colors")]
+        public Color BoltColor;
+        public Color MenuColor;
+        public Color NameplateColor;
+        public Color KartColor;
+        public Color KillFeedEntryColor;
+        public Color ItemsColor;
+    }
+}
