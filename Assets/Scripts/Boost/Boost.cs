7e04c705c0e5e248a27bd4eb1466790e747bc0db
Shashimee <simon.trebern@outlook.com>
2019-01-23 16:58:04 +0100

    develop : Fixed ground & item boxes places
    Former-commit-id: eceba9aea65ee906b9b1e027677e459d7c7a3582

--- Assets/Scripts/Boost/Boost.cs
+++ Assets/Scripts/Boost/Boost.cs
@@ -66,7 +66,7 @@ namespace Common.PhysicsUtils
                 settings.SecondsToDecreaseClampSpeed));
         }
 
-        public void StopAllCurrentCustomBoost(BoostSettings settings)
+        public void StopAllCurrentCustomBoost()
         {
             StopAllCoroutines();
             _clampSpeed.CurrentClampSpeed = _clampSpeed.BaseClampSpeed;
