b42349f57e73f37205829809ee88002ae47a22f9
Shashimee <simon.trebern@outlook.com>
2019-01-25 19:20:03 +0100

    develop : Added game countdown & removed lobby countdown

---
+++ Assets/Scripts/PhotonCloud/Lobby/LobbySettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Photon
+{
+    [CreateAssetMenu(menuName = "Lobby Settings/Global Settings")]
+    public class LobbySettings : ScriptableObject
+    {
+        [Header("Countdown Before Game")]
+        public bool Countdown;
+        public float CountdownSeconds;
+    }
+}
