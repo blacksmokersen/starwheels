2efa1c49f6275d76c32fdc7f1260562227a86860
Shashimee <simon.trebern@outlook.com>
2019-01-26 15:38:21 +0100

    422 : Added shield to merge
    Former-commit-id: 88b88efd79024f5415f94adf73bc61f8a218e751

---
+++ Assets/Scripts/Items/Merge/ItemMergingSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Items.Merge
+{
+    [CreateAssetMenu(menuName = "Kart Settings/Merge Settings")]
+    public class ItemMergingSettings : ScriptableObject
+    {
+        public float FullMergeBoostSeconds;
+        public float FullMergeShieldSeconds;
+        public float SmallMergeBoostSeconds;
+        public float SmallMergeShieldSeconds;
+    }
+}
