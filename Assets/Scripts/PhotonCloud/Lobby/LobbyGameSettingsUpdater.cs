6647ee4c07e2c94b3b90be6c213e8f88785595ba
Shashimee <simon.trebern@outlook.com>
2019-01-27 21:39:50 +0100

    404 : New scripts and first tests

---
+++ Assets/Scripts/PhotonCloud/Lobby/LobbyGameSettingsUpdater.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Bolt;
+using TMPro;
+using MyExtensions;
+
+namespace Photon.Lobby
+{
+    public class LobbyGameSettingsUpdater : EntityBehaviour<ILobbyGameState>
+    {
+        [Header("UI Elements")]
+        [SerializeField] private TMP_Dropdown _gameModeDropdown;
+        [SerializeField] private TMP_Dropdown _mapDropdown;
+
+        [Header("Unity Events")]
+        public UnityEvent OnGameModeUpdated;
+        public UnityEvent OnMapUpdated;
+        public UnityEvent OnMaxPlayerCountUpdated;
+        public UnityEvent OnPlayerCountUpdated;
+        public UnityEvent OnPublicGameUpdated;
+
+        private GameSettings _gameSettings;
+
+        // CORE
+
+        private void Awake()
+        {
+            _gameSettings = Resources.Load<GameSettings>("GameSettings");
+            Debug.Log(_gameSettings == null);
+
+            if (BoltNetwork.IsServer)
+            {
+                _gameModeDropdown.onValueChanged.AddListener((i) =>
+                {
+                    state.GameMode = _gameModeDropdown.options[i].text;
+                });
+
+                _mapDropdown.onValueChanged.AddListener((i) =>
+                {
+                    state.Map = _mapDropdown.options[i].text;
+                });
+            }
+        }
+
+        // BOLT
+
+        public override void Attached()
+        {
+            state.AddCallback("GameMode", UpdateGameMode);
+            state.AddCallback("MapName", UpdateMap);
+            state.AddCallback("MaxPlayerCount", UpdateMaxPlayerCount);
+            state.AddCallback("PublicGame", UpdatePublicGame);
+
+            state.GameMode = _gameModeDropdown.options[_gameModeDropdown.value].text;
+            state.Map = _mapDropdown.options[_gameModeDropdown.value].text;
+        }
+
+        // PRIVATE
+
+        private void UpdateGameMode()
+        {
+            Debug.Log("Updating game mode");
+            _gameModeDropdown.ChangeTMProDropdownValue(state.GameMode);
+            _gameSettings.GameMode = state.GameMode;
+
+            if(OnGameModeUpdated != null) OnGameModeUpdated.Invoke();
+        }
+
+        private void UpdateMap()
+        {
+            _mapDropdown.ChangeTMProDropdownValue(state.Map);
+            _gameSettings.MapName = state.Map;
+
+            if (OnMapUpdated != null) OnMapUpdated.Invoke();
+        }
+
+        private void UpdateMaxPlayerCount()
+        {
+            if (OnMaxPlayerCountUpdated != null) OnMaxPlayerCountUpdated.Invoke();
+        }
+
+        private void UpdateCurrentPlayerCount()
+        {
+            if (OnPlayerCountUpdated != null) OnPlayerCountUpdated.Invoke();
+        }
+
+        private void UpdatePublicGame()
+        {
+            if (OnPublicGameUpdated != null) OnPublicGameUpdated.Invoke();
+        }
+    }
+}
