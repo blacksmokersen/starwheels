6f10abf6c320291d9902cdb0588d422b609bd63f
Wydman <baptiste.int@gmail.com>
2019-01-29 20:13:31 +0100

    424 : Added KartMeshDisabler Tools + Cloak Upgrade

--- Assets/Scripts/Abilities/Cloak/CloakPortalBehaviour.cs
+++ Assets/Scripts/Abilities/Cloak/CloakPortalBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using Bolt;
 using UnityEngine;
 
@@ -9,26 +10,45 @@ namespace Abilities
     {
         [SerializeField] GameObject _targetPortal;
 
+        [Header("Unity Events")]
+        public UnityEvent AtStartCloakTeleport;
+        public UnityEvent AtEndCloakTeleport;
+
+        private KartMeshDisabler _kartMeshDisabler;
+        private Health.Health _health;
+
         public void TeleportPlayerToTargetPortal(GameObject kart,GameObject targetPortal)
         {
-
-
-
+            _kartMeshDisabler = kart.GetComponentInChildren<CloakAbility>().KartMeshDisabler;
+            _health = kart.GetComponentInChildren<Health.Health>();
+            StartCoroutine(PortalTravelBehaviour(kart, targetPortal));
         }
-
-
-
-
 
         private void OnTriggerEnter(Collider other)
         {
-            if (entity.isAttached && entity.isOwner && other.CompareTag(Constants.Tag.KartHealthHitBox))
+            if (other.CompareTag(Constants.Tag.Kart))
             {
-                if (other.gameObject.GetComponentInChildren<CloakAbility>().CanUsePortals)
+                Debug.Log(other.transform.root.gameObject);
+                if (other.transform.root.gameObject.GetComponentInChildren<CloakAbility>().CanUsePortals)
                 {
-                    TeleportPlayerToTargetPortal(other.gameObject,_targetPortal);
+                    TeleportPlayerToTargetPortal(other.transform.root.gameObject, _targetPortal);
                 }
             }
         }
+
+        private IEnumerator PortalTravelBehaviour(GameObject kart, GameObject targetPortal)
+        {
+            _kartMeshDisabler.DisableKartMeshes(false);
+            _health.SetInvincibility();
+            yield return new WaitForSeconds(0.5f);
+            _kartMeshDisabler.EnableKartMeshes(false);
+            _health.UnsetInvincibility();
+            /*
+            var y = _tpBack.transform.position.y + 5f;
+            _rb.transform.position = new Vector3(_tpBack.transform.position.x, y, _tpBack.transform.position.z);
+            _rb.transform.rotation = GetKartRotation();
+            */
+        }
+
     }
 }
