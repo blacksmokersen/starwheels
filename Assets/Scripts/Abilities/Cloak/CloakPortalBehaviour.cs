c593b85dc13ce1a7c334c62005fdbf739287274e
Wydman <baptiste.int@gmail.com>
2019-01-29 17:52:53 +0100

    424 : Base CloakRework Structure
    Former-commit-id: 0337bce150f50b15ffc729658d7b377c3fed109c

---
+++ Assets/Scripts/Abilities/Cloak/CloakPortalBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+using UnityEngine;
+
+namespace Abilities
+{
+    public class CloakPortalBehaviour : EntityBehaviour<IItemState>
+    {
+        [SerializeField] GameObject _targetPortal;
+
+        public void TeleportPlayerToTargetPortal(GameObject kart,GameObject targetPortal)
+        {
+
+
+
+        }
+
+
+
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (entity.isAttached && entity.isOwner && other.CompareTag(Constants.Tag.KartHealthHitBox))
+            {
+                if (other.gameObject.GetComponentInChildren<CloakAbility>().CanUsePortals)
+                {
+                    TeleportPlayerToTargetPortal(other.gameObject,_targetPortal);
+                }
+            }
+        }
+    }
+}
