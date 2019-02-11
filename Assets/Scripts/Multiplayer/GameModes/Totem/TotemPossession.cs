8b106edaced10364e79c918e87b2ba173e12f196
Wydman <baptiste.int@gmail.com>
2019-02-11 19:23:11 +0100

    develop : Added TotemChargeHUD
    Former-commit-id: 4967da9511b76f48614818c4753a5f104942f800

--- Assets/Scripts/Multiplayer/GameModes/Totem/TotemPossession.cs
+++ Assets/Scripts/Multiplayer/GameModes/Totem/TotemPossession.cs
@@ -25,14 +25,18 @@ namespace GameModes.Totem
 
         private bool _referencesSet = false;
 
+        private TotemChargeHUD _totemChargeHUD;
+
         // CORE
 
         private void Start()
         {
             StartCoroutine(SynchronizationRoutine());
+            _totemChargeHUD = GameObject.Find("TotemCharge").GetComponent<TotemChargeHUD>();
         }
 
         // BOLT
+
 
         public override void OnEvent(TotemThrown evnt)
         {
@@ -54,6 +58,7 @@ namespace GameModes.Totem
                 {
                     _isLocalOwner = false;
                     OnTotemLost.Invoke();
+                    _totemChargeHUD.ShowChargingHUD();
                 }
             }
         }
@@ -75,6 +80,7 @@ namespace GameModes.Totem
                     {
                         _isLocalOwner = false;
                         OnTotemLost.Invoke();
+                        _totemChargeHUD.ShowChargingHUD();
                     }
                 }
             }
@@ -116,11 +122,13 @@ namespace GameModes.Totem
                 {
                     _isLocalOwner = true;
                     OnTotemGet.Invoke();
+                    _totemChargeHUD.ShowDischargingHUD();
                 }
                 else if (_isLocalOwner) // If I was the old owner of the totem
                 {
                     _isLocalOwner = false;
                     OnTotemLost.Invoke();
+                    _totemChargeHUD.ShowChargingHUD();
                 }
             }
             else
