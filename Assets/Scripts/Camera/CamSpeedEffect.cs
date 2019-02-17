8954b52b2eb4e25c7e1622775f326f119b9d38cb
Shashimee <simon.trebern@outlook.com>
2019-02-17 17:30:58 +0100

    develop : ObserversSetup & small folder organization
    Former-commit-id: b29021ea1e7da0f8a5342b51b8c854d0afa53112

--- Assets/Scripts/Camera/CamSpeedEffect.cs
+++ Assets/Scripts/Camera/CamSpeedEffect.cs
@@ -3,35 +3,42 @@ using Cinemachine;
 
 namespace CameraUtils
 {
-    public class CamSpeedEffect : MonoBehaviour
+    public class CamSpeedEffect : MonoBehaviour, IObserver
     {
+        [Header("Effect Settings")]
         [SerializeField] private float _fov = 50f;
 
         private CinemachineVirtualCamera _cinemachine;
         private Rigidbody _rigidbody;
+
+        // CORE
 
         private void Awake()
         {
             _cinemachine = GetComponent<CinemachineVirtualCamera>();
         }
 
-        void Update()
+        private void Update()
         {
             SpeedOnCamBehaviour();
         }
 
-        public void SpeedOnCamBehaviour()
+        // PUBLIC
+
+        public void Observe(GameObject kartRoot)
+        {
+            _rigidbody = kartRoot.GetComponent<Rigidbody>();
+        }
+
+        // PRIVATE
+
+        private void SpeedOnCamBehaviour()
         {
             if (_rigidbody)
             {
                 float clampCam = Mathf.Clamp(_rigidbody.velocity.magnitude / 5, 0, 20);
                 _cinemachine.m_Lens.FieldOfView = _fov + clampCam;
             }
-        }
-
-        public void SetRigidbody(Rigidbody rb)
-        {
-            _rigidbody = rb;
         }
     }
 }
