36585a22caa7fce6caabf2192d8a31136f86e8bd
Shashimee <simon.trebern@outlook.com>
2019-02-17 18:10:55 +0100

    develop [fix] : Fixed rocket circles

--- Assets/Scripts/Items/ItemsScripts/Rocket/RocketBehaviour.cs
+++ Assets/Scripts/Items/ItemsScripts/Rocket/RocketBehaviour.cs
@@ -11,11 +11,13 @@ namespace Items
         public float QuickTurnSpeed;
         public float QuickTurnDuration;
         public float AntiShakingThreshold;
+        public float SecondsBeforeIgnoringTarget;
 
         private float _actualTurnSpeed;
+        private bool _ignoreTarget = false;
         private RocketLockTarget _rocketLock;
 
-        // CORE
+        // MONO
 
         private new void Awake()
         {
@@ -23,42 +25,16 @@ namespace Items
             _rocketLock = GetComponentInChildren<RocketLockTarget>();
         }
 
+        private void Start()
+        {
+            StartCoroutine(IgnoreTargetAfterXSeconds(SecondsBeforeIgnoringTarget));
+        }
+
         private new void FixedUpdate()
         {
             TurnTowardTarget();
             SetVelocityForward();
             base.FixedUpdate();
-        }
-
-        // PUBLIC
-
-        public void NoCircleSecurityQuickTurn()
-        {
-            _actualTurnSpeed = 1000;
-        }
-
-        // PRIVATE
-
-        private void SetVelocityForward()
-        {
-            var newVelocity = transform.forward;
-            newVelocity.y = rb.velocity.y;
-            rb.velocity = newVelocity;
-        }
-
-        private void TurnTowardTarget()
-        {
-            if (_rocketLock.CurrentTarget != null)
-            {
-                if (transform.InverseTransformPoint(_rocketLock.CurrentTarget.transform.position).x < -AntiShakingThreshold) // If the target is on the left we turn to the left
-                {
-                    transform.Rotate(Vector3.down * _actualTurnSpeed * Time.deltaTime);
-                }
-                else if (transform.InverseTransformPoint(_rocketLock.CurrentTarget.transform.position).x > AntiShakingThreshold)  // If the target is on the right we turn to the right
-                {
-                    transform.Rotate(Vector3.up * _actualTurnSpeed * Time.deltaTime);
-                }
-            }
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -73,11 +49,42 @@ namespace Items
             }
         }
 
+        // PUBLIC
+
         public IEnumerator StartQuickTurn()
         {
             _actualTurnSpeed = QuickTurnSpeed;
             yield return new WaitForSeconds(QuickTurnDuration);
             _actualTurnSpeed = NormalTurnSpeed;
+        }
+        // PRIVATE
+
+        private void SetVelocityForward()
+        {
+            var newVelocity = transform.forward;
+            newVelocity.y = rb.velocity.y;
+            rb.velocity = newVelocity;
+        }
+
+        private void TurnTowardTarget()
+        {
+            if (_rocketLock.CurrentTarget != null && _ignoreTarget == false)
+            {
+                if (transform.InverseTransformPoint(_rocketLock.CurrentTarget.transform.position).x < -AntiShakingThreshold) // If the target is on the left we turn to the left
+                {
+                    transform.Rotate(Vector3.down * _actualTurnSpeed * Time.deltaTime);
+                }
+                else if (transform.InverseTransformPoint(_rocketLock.CurrentTarget.transform.position).x > AntiShakingThreshold)  // If the target is on the right we turn to the right
+                {
+                    transform.Rotate(Vector3.up * _actualTurnSpeed * Time.deltaTime);
+                }
+            }
+        }
+
+        private IEnumerator IgnoreTargetAfterXSeconds(float x)
+        {
+            yield return new WaitForSeconds(x);
+            _ignoreTarget = true;
         }
     }
 }
