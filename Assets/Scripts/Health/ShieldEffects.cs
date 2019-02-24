b5782be39e2f25740013d5443fd23be980b20eff
Shashimee <simon.trebern@outlook.com>
2019-02-24 15:24:09 +0100

    404 : Reworked merge (shield & boost)

--- Assets/Scripts/Health/ShieldEffects.cs
+++ Assets/Scripts/Health/ShieldEffects.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using UnityEngine;
+using Items;
 
 namespace Health
 {
     public class ShieldEffects : MonoBehaviour
     {
+        [SerializeField] private Health _health;
+
         public void Activate()
         {
             gameObject.SetActive(true);
@@ -13,12 +16,15 @@ namespace Health
         public void ActivateForXSeconds(float x)
         {
             Activate();
+            _health.SetInvincibilityForXSeconds(x);
             StartCoroutine(DeactivateAfterXSecondsRoutine(x));
         }
 
         public void Deactivate()
         {
             gameObject.SetActive(false);
+            _health.UnsetInvincibility();
+            StopAllCoroutines();
         }
 
         // PRIVATE
@@ -27,6 +33,27 @@ namespace Health
         {
             yield return new WaitForSeconds(x);
             Deactivate();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (BoltNetwork.IsServer)
+            {
+                if (other.gameObject.CompareTag(Constants.Tag.ItemCollisionHitBox)) // It is an item collision
+                {
+                    var itemCollisionTrigger = other.GetComponent<ItemCollisionTrigger>();
+
+                    if (itemCollisionTrigger.ItemCollision.HitsPlayer) // It is an item that damages the player
+                    {
+                        StartCoroutine(DeactivateAfterXSecondsRoutine(.1f));
+                    }
+                }
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            // TODO OVERCHARGE
         }
     }
 }
