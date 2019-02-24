7ed813183328b1522a9cec93351436f70d3144a6
Shashimee <simon.trebern@outlook.com>
2019-02-24 20:49:04 +0100

    404 : Random legendary upgrade
    Former-commit-id: 5dd2e6bc9e76f571918f6b34ef215505d16a7d42

---
+++ Assets/Scripts/Items/Lottery/LegendaryItemBoxSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Items.Lottery
+{
+    [CreateAssetMenu(menuName = "Item Settings/Legendary Upgrade")]
+    public class LegendaryItemBoxSpawner : MonoBehaviour
+    {
+        [SerializeField] private LegendaryItemBoxesSpawnerSettings _settings;
+
+        private ItemBox[] _itemBoxes;
+
+        private void Start()
+        {
+            _itemBoxes = FindObjectsOfType<ItemBox>();
+
+            if (_settings.UpgradeEveryXSeconds)
+            {
+                StartCoroutine(UpgradeOneItemBoxRoutine());
+            }
+        }
+
+        private void UpgradeOneItemBox()
+        {
+            Debug.Log("Upgrading one");
+            _itemBoxes[Random.Range(0, _itemBoxes.Length)].UpgradeToNext();
+        }
+
+        private void ResetAllItemBoxes()
+        {
+            foreach (var itemBox in _itemBoxes)
+            {
+                itemBox.ResetToFirstUpgrade();
+            }
+        }
+
+        private IEnumerator UpgradeOneItemBoxRoutine()
+        {
+            while (_settings.UpgradeEveryXSeconds)
+            {
+                yield return new WaitForSeconds(_settings.SecondsBetweenUpgrades);
+                ResetAllItemBoxes();
+                UpgradeOneItemBox();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Items/Lottery/LegendaryItemBoxesSpawnerSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Items.Lottery
+{
+    [CreateAssetMenu(menuName = "Item Settings/Legendary Upgrade")]
+    public class LegendaryItemBoxesSpawnerSettings : ScriptableObject
+    {
+        public bool UpgradeEveryXSeconds;
+        public float SecondsBetweenUpgrades;
+    }
+}
