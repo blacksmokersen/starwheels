4fcd63d51fd2496c2ce7dccff68a9ff0071cb0ec
Shashimee <simon.trebern@outlook.com>
2019-02-26 14:33:48 +0100

    develop : Fixed battle errors on destroy
    Former-commit-id: 1aa6e4af35bd22b360c9b539959332fd9fddaaa3

--- Assets/Scripts/Multiplayer/Spawns/GameReadyEventListener.cs
+++ Assets/Scripts/Multiplayer/Spawns/GameReadyEventListener.cs
@@ -9,11 +9,31 @@ namespace Multiplayer
         [Header("Unity Events")]
         public UnityEvent OnGameReady;
 
+        [Header("Game Started")]
+        [SerializeField] private BoolVariable _gameStartedVariable;
+
+        // CORE
+
+        private void Start()
+        {
+            if (_gameStartedVariable.Value)
+            {
+                if (OnGameReady != null)
+                {
+                    OnGameReady.Invoke();
+                }
+            }
+        }
+
         // BOLT
 
         public override void OnEvent(GameReady evnt)
         {
-            OnGameReady.Invoke();
+            if (OnGameReady != null)
+            {
+                OnGameReady.Invoke();
+            }
+            _gameStartedVariable.Value = true;
         }
 
         public override void OnEvent(LobbyCountdown evnt)
