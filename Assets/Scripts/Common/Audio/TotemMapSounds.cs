d0ebbd6ff7d2b4d9e4c69befb156e18322c4e892
Wydman <baptiste.int@gmail.com>
2019-02-28 18:43:25 +0100

    develop : new totem sounds pt2

---
+++ Assets/Scripts/Common/Audio/TotemMapSounds.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Multiplayer.Teams;
+
+public class TotemMapSounds : GlobalEventListener
+{
+
+    [SerializeField] private AudioSource _blueStep1;
+    [SerializeField] private AudioSource _blueStep2;
+    [SerializeField] private AudioSource _blueStep3;
+    [SerializeField] private AudioSource _blueStep4Win;
+    [SerializeField] private AudioSource _redStep1;
+    [SerializeField] private AudioSource _redStep2;
+    [SerializeField] private AudioSource _redStep3;
+    [SerializeField] private AudioSource _redStep4Win;
+
+    private int _wallHitIterationBlue = 0;
+    private int _wallHitIterationRed = 0;
+
+    public override void OnEvent(TotemWallHit totemWallHit)
+    {
+        if (totemWallHit.Team == TeamsColors.RedColor)
+        {
+            _wallHitIterationRed++;
+            switch (_wallHitIterationRed)
+            {
+                case 1:
+                    _redStep1.Play();
+                    break;
+                case 2:
+                    _redStep2.Play();
+                    break;
+                case 3:
+                    _redStep4Win.Play();
+                    break;
+            }
+        }
+        else
+        {
+            _wallHitIterationBlue++;
+            switch (_wallHitIterationBlue)
+            {
+                case 1:
+                    _blueStep1.Play();
+                    break;
+                case 2:
+                    _blueStep2.Play();
+                    break;
+                case 3:
+                    _blueStep4Win.Play();
+                    break;
+            }
+        }
+    }
+}
