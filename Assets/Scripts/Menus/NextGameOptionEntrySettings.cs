9dd8ec7c8f0685f3408b16677d5436a99edaf6f4
Shashimee <simon.trebern@outlook.com>
2019-03-07 20:01:25 +0100

    463 : Next game menu prefab & code skeleton

---
+++ Assets/Scripts/Menus/NextGameOptionEntrySettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class NextGameOptionEntrySettings : ScriptableObject
+    {
+        public StringVariable OptionName;
+        public float SecondsBeforeNextOption;
+        public string Choice;
+    }
+}
