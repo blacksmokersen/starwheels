f1ec213e2bb90fded7f5cab15c2e721ca09b023f
Shashimee <simon.trebern@outlook.com>
2019-03-07 20:01:25 +0100

    463 : Next game menu prefab & code skeleton
    Former-commit-id: 9dd8ec7c8f0685f3408b16677d5436a99edaf6f4

---
+++ Assets/Scripts/Menus/NextGameOption.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    public class NextGameOption : MonoBehaviour
+    {
+        public NextGameOptionEntrySettings Settings;
+
+        public void SetActive()
+        {
+
+        }
+
+        public void GotoNext()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/NextGamePanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+    public class NextGamePanel : MonoBehaviour
+    {
+
+    }
+}
