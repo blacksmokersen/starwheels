b8bccf93b2b0ae20843481cfb4a5bb198cd5934e
Shashimee <simon.trebern@outlook.com>
2019-03-09 17:51:13 +0100

    463 : Continued prototype
    Former-commit-id: 3b7e83afd8d5d463d448147b98832c38db7f3e9e

--- Assets/Scripts/Menus/NextGameOption.cs
+++ Assets/Scripts/Menus/NextGameOption.cs
@@ -1,19 +1,99 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 namespace Menu
 {
     public class NextGameOption : MonoBehaviour
     {
+        [Header("Settings")]
         public NextGameOptionEntrySettings Settings;
+        [SerializeField] private NextGameOptionColors ColorSettings;
+
+        [Header("UI Elements")]
+        [SerializeField] private HorizontalLayoutGroup _choicesLayout;
+        [SerializeField] private Image _thisChoiceBackground;
+        [SerializeField] private TextMeshProUGUI _optionText;
+
+        [Header("Events")]
+        [Tooltip("Value is between 0 and 1 for the completion percentage of this option.")]
+        public FloatEvent OnEntryTimeUpdate;
+        public StringEvent OnOptionChosen;
+
+        private float timer = 0f;
+
+        // CORE
+
+        private void Awake()
+        {
+            _thisChoiceBackground.color = ColorSettings.NotChosenColor;
+        }
+
+        private void OnEnable()
+        {
+            if (Settings.IsFirstOptionInMenu)
+            {
+                SetActive();
+            }
+        }
+
+        // PUBLIC
 
         public void SetActive()
         {
-
+            InitializeListeners();
+            _thisChoiceBackground.color = ColorSettings.HighlightedColor;
         }
 
-        public void GotoNext()
+        public void SetChoice(string value)
         {
+            Settings.Choice = value;
+            _thisChoiceBackground.color = ColorSettings.ChosenColor;
+            OnOptionChosen.Invoke(Settings.Choice);
+        }
 
+        // PRIVATE
+
+        private void InitializeListeners()
+        {
+            _choicesLayout.gameObject.SetActive(true);
+            var toggles = _choicesLayout.GetComponentsInChildren<Toggle>();
+
+            foreach (var toggle in toggles)
+            {
+                var choice = toggle.GetComponent<Label>().String.Value;
+                toggle.onValueChanged.AddListener((b) =>
+                {
+                    SetChoice(choice);
+                    HideChoices();
+                });
+            }
+        }
+
+        private void UpdateTimer(float secondsLeft)
+        {
+            _optionText.text = Settings.OptionName + " : " + secondsLeft;
+        }
+
+        private IEnumerator UpdateTimeRoutine()
+        {
+            while (timer < Settings.SecondsBeforeNextOption)
+            {
+                var delta = Time.deltaTime;
+                timer += delta;
+                UpdateTimer((int)(Settings.SecondsBeforeNextOption - timer));
+                OnEntryTimeUpdate.Invoke(timer / Settings.SecondsBeforeNextOption);
+                yield return new WaitForEndOfFrame();
+            }
+            OnEntryTimeUpdate.Invoke(1f);
+            HideChoices();
+        }
+
+        private void HideChoices()
+        {
+            _choicesLayout.gameObject.SetActive(false);
+            StopAllCoroutines();
         }
     }
 }
--- Assets/Scripts/Menus/NextGamePanel.cs
+++ Assets/Scripts/Menus/NextGamePanel.cs
@@ -1,11 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using SW.Matchmaking;
 
 namespace Menu
 {
     public class NextGamePanel : MonoBehaviour
     {
+        [Header("Lobby Info")]
+        [SerializeField] private LobbyData _lobbyData;
 
+        private string _nextGamemode;
+        private string _nextMap;
+
+        // PUBLIC
+
+        public void StartNextGame()
+        {
+            if (BoltNetwork.IsServer)
+            {
+                SWMatchmaking.SetLobbyData(_lobbyData);
+                BoltNetwork.LoadScene(_nextMap);
+            }
+        }
+
+        // PRIVATE
+
+        private void SetNextGamemode(string nextGamemode)
+        {
+            _lobbyData.ChosenGamemode = nextGamemode;
+        }
+
+        private void SetNextMap(string nextMap)
+        {
+            _lobbyData.ChosenMapName = nextMap;
+        }
+
+        private void SetNextSkin(string nextSkin)
+        {
+
+        }
     }
 }
