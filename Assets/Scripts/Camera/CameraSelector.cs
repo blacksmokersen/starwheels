4768a3bbd07c4240f4cb38145b9ae220a62f850a
Wydman <baptiste.int@gmail.com>
2019-03-16 23:28:07 +0100

    474 : Camera Selector and some Map Cam Prefabs

---
+++ Assets/Scripts/Camera/CameraSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSelector : MonoBehaviour
+{
+    public GameObject ActualCamera;
+
+    [SerializeField] private GameObject _kartCamera;
+    [SerializeField] private GameObject _mapCamera;
+    [SerializeField] private GameObject _ionbeamCamera;
+
+    private void Awake()
+    {
+        _mapCamera = GameObject.FindWithTag("MapCamera");
+    }
+
+
+
+}
