a8dbe90d5b7b893304a01489e0b9d0972d904cc7
Wydman <baptiste.int@gmail.com>
2019-03-20 17:28:09 +0100

    474-BIS : CameraManagement +  Changed CountdownSO
    Former-commit-id: cf2d0ff03c651b2a8dbb233b0a49914ceadceb78

--- Assets/Scripts/Camera/CameraSelector.cs
+++ Assets/Scripts/Camera/CameraSelector.cs
@@ -1,20 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
-public class CameraSelector : MonoBehaviour
+namespace CameraUtils
 {
-    public GameObject ActualCamera;
-
-    [SerializeField] private GameObject _kartCamera;
-    [SerializeField] private GameObject _mapCamera;
-    [SerializeField] private GameObject _ionbeamCamera;
-
-    private void Awake()
+    public class CameraSelector : GlobalEventListener
     {
-        _mapCamera = GameObject.FindWithTag("MapCamera");
+        public GameObject ActualCamera;
+
+        [SerializeField] private Camera _kartCamera;
+        [SerializeField] private Camera _mapCamera;
+
+        [SerializeField] private CountdownSettings _countdownSettings;
+
+        //CORE
+
+        private void Start()
+        {
+            if (!_countdownSettings.Countdown)
+                ShowPlayerCamera();
+            if (_mapCamera == null)
+            {
+                Debug.LogWarning("There is no MapCamera selected in the CameraSelector");
+                _kartCamera.enabled = true;
+            }
+            if (_kartCamera == null)
+                Debug.LogError("There is no PlayerCamera selected in the CameraSelector");
+        }
+
+        //BOLT
+
+        public override void OnEvent(LobbyCountdown evnt)
+        {
+            if (evnt.Time == 3)
+            {
+                ShowPlayerCamera();
+            }
+        }
+
+        //PRIVATE
+
+        private void ShowPlayerCamera()
+        {
+            _mapCamera.enabled = false;
+            _kartCamera.enabled = true;
+        }
+
+        private void ShowMapCamera()
+        {
+            _mapCamera.enabled = true;
+            _kartCamera.enabled = false;
+        }
+
+
+        //DEBUG
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                _mapCamera.enabled = false;
+                _kartCamera.enabled = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+                _mapCamera.enabled = true;
+                _kartCamera.enabled = false;
+            }
+        }
+
+
+
+
+
     }
-
-
-
 }
---
+++ Assets/Scripts/Gamemodes/CountdownSettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gamemodes/CountdownSettings")]
+public class CountdownSettings : ScriptableObject
+{
+    public bool Countdown;
+    public float Timer;
+}
