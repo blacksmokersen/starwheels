52ced3f43e917932a4455dfb8e481c8ad499a58d
Wydman <baptiste.int@gmail.com>
2019-03-26 14:45:21 +0100

    develop [Feature] : Added FreeCamera
    Keypad 9 To activate free Cam
    Keypad 8 To activate map Cam
    Keypad 7 To activate Player Cam

---
+++ Assets/Scripts/Camera/FreeCamera.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Engine;
+
+namespace CameraUtils
+{
+    public class FreeCamera : MonoBehaviour, IControllable, IObserver
+    {
+        [SerializeField] private bool _enabled = true;
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set { _enabled = value; }
+        }
+
+        [SerializeField] private float _horizontalSpeed;
+        [SerializeField] private float _verticalSpeed;
+        [SerializeField] private float _rotateYSpeed;
+        [SerializeField] private float _rotateXSpeed;
+        [SerializeField] private float _forwardSpeed;
+        [SerializeField] private float _backwardSpeed;
+
+         private Camera _camera;
+         private GameObject _kart;
+
+        //CORE
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            MapInputs();
+        }
+
+        //PUBLIC
+
+        public void Observe(GameObject kartroot)
+        {
+            _kart = kartroot;
+        }
+
+        public void MapInputs()
+        {
+            if (Enabled)
+            {
+                transform.eulerAngles += new Vector3(0,Input.GetAxis(Constants.Input.TurnCamera) * _rotateXSpeed,0);
+                transform.eulerAngles += new Vector3(-Input.GetAxis(Constants.Input.UpAndDownCamera) * _rotateYSpeed,0,0);
+
+                transform.Translate(Vector3.forward * Input.GetAxis(Constants.Input.Accelerate) * _forwardSpeed);
+                transform.Translate(Vector3.back * Input.GetAxis(Constants.Input.Decelerate) * _backwardSpeed);
+                transform.Translate(Vector3.right * Input.GetAxis(Constants.Input.TurnAxis) * _horizontalSpeed);
+                transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.UpAndDownAxis) * _verticalSpeed);
+            }
+        }
+
+        public void DisableKartControls()
+        {
+            _kart.GetComponentInChildren<EngineBehaviour>().Enabled = false;
+        }
+        public void EnableKartControls()
+        {
+            _kart.GetComponentInChildren<EngineBehaviour>().Enabled = true;
+        }
+    }
+}
