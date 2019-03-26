900982350b9abd52e54d0d1b89ff2e7c2e2a4cbc
Wydman <baptiste.int@gmail.com>
2019-03-26 15:43:12 +0100

    develop : Added Switch Inputs for FreeCamera
    Former-commit-id: c92fe613c32443a0461b9ccf1d20751798b10d28

--- Assets/Scripts/Camera/FreeCamera.cs
+++ Assets/Scripts/Camera/FreeCamera.cs
@@ -14,6 +14,8 @@ namespace CameraUtils
             set { _enabled = value; }
         }
 
+        [SerializeField] private bool _switchInputs;
+
         [SerializeField] private float _horizontalSpeed;
         [SerializeField] private float _verticalSpeed;
         [SerializeField] private float _rotateYSpeed;
@@ -21,8 +23,8 @@ namespace CameraUtils
         [SerializeField] private float _forwardSpeed;
         [SerializeField] private float _backwardSpeed;
 
-         private Camera _camera;
-         private GameObject _kart;
+        private Camera _camera;
+        private GameObject _kart;
 
         //CORE
 
@@ -47,13 +49,23 @@ namespace CameraUtils
         {
             if (Enabled)
             {
-                transform.eulerAngles += new Vector3(0,Input.GetAxis(Constants.Input.TurnCamera) * _rotateXSpeed,0);
-                transform.eulerAngles += new Vector3(-Input.GetAxis(Constants.Input.UpAndDownCamera) * _rotateYSpeed,0,0);
+                transform.eulerAngles += new Vector3(0, Input.GetAxis(Constants.Input.TurnCamera) * _rotateXSpeed, 0);
+                transform.eulerAngles += new Vector3(-Input.GetAxis(Constants.Input.UpAndDownCamera) * _rotateYSpeed, 0, 0);
 
-                transform.Translate(Vector3.forward * Input.GetAxis(Constants.Input.Accelerate) * _forwardSpeed);
-                transform.Translate(Vector3.back * Input.GetAxis(Constants.Input.Decelerate) * _backwardSpeed);
-                transform.Translate(Vector3.right * Input.GetAxis(Constants.Input.TurnAxis) * _horizontalSpeed);
-                transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.UpAndDownAxis) * _verticalSpeed);
+                if (_switchInputs)
+                {
+                    transform.Translate(Vector3.forward * Input.GetAxis(Constants.Input.Accelerate) * _forwardSpeed);
+                    transform.Translate(Vector3.back * Input.GetAxis(Constants.Input.Decelerate) * _backwardSpeed);
+                    transform.Translate(Vector3.right * Input.GetAxis(Constants.Input.TurnAxis) * _horizontalSpeed);
+                    transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.UpAndDownAxis) * _verticalSpeed);
+                }
+                else
+                {
+                    transform.Translate(Vector3.forward * Input.GetAxis(Constants.Input.UpAndDownAxis) * _forwardSpeed);
+                    transform.Translate(Vector3.right * Input.GetAxis(Constants.Input.TurnAxis) * _horizontalSpeed);
+                    transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.Accelerate) * _verticalSpeed);
+                    transform.Translate(Vector3.down * Input.GetAxis(Constants.Input.Decelerate) * _verticalSpeed);
+                }
             }
         }
 
