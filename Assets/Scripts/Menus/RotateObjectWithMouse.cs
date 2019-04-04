a30d337c583aad1a312fe66ca71ced20261e6996
Shashimee <simon.trebern@outlook.com>
2019-04-04 18:58:41 +0200

    develop : Added Kart Rotator on main menu
    Former-commit-id: f6b1b8f5d8f418179675cb836a6771d34581c872

---
+++ Assets/Scripts/Menus/RotateObjectWithMouse.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    [DisallowMultipleComponent]
+    public class RotateObjectWithMouse : MonoBehaviour
+    {
+        [Header("Target")]
+        [SerializeField] private GameObject _targetObject;
+
+        [Header("Settings")]
+        [SerializeField] private float _manualSpeed = 1f;
+        [SerializeField] private float _automaticSpeed = 1f;
+
+        private bool _canRotateObject = false;
+
+        // CORE
+
+        private void Update()
+        {
+            SlowlyRotateObject();
+            CheckMouseInputs();
+            CheckJoystickInputs();
+        }
+
+        // PRIVATE
+
+        private void CheckMouseInputs()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (gameObject.activeSelf && RectTransformUtility.RectangleContainsScreenPoint(gameObject.GetComponent<RectTransform>(), Input.mousePosition, null))
+                {
+                    _canRotateObject = true;
+                }
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                if (_canRotateObject)
+                {
+                    _targetObject.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * _manualSpeed);
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _canRotateObject = false;
+            }
+        }
+
+        private void CheckJoystickInputs()
+        {
+            _targetObject.transform.Rotate(new Vector3(0, Input.GetAxis(Constants.Input.TurnCamera), 0) * Time.deltaTime * _manualSpeed);
+        }
+
+        private void SlowlyRotateObject()
+        {
+            _targetObject.transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * _automaticSpeed);
+        }
+    }
+}
