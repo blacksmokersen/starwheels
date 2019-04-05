aa457ecd2d7c2e8402f13c8a8f2df8cb8d14aa53
Shashimee <simon.trebern@outlook.com>
2019-04-05 17:43:07 +0200

    develop : Finished totem GM & Reworked Factory

---
+++ Assets/Scripts/Gamemodes/Totem/OnMap/NexusCollisionHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Gamemodes.Totem
+{
+    [DisallowMultipleComponent]
+    public class NexusCollisionHandler : MonoBehaviour
+    {
+        [Header("Data")]
+        public Team OwnerTeam;
+
+        [Header("Events")]
+        public TeamEvent OnNexusDestroyed;
+
+        // CORE
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (BoltNetwork.IsServer && collision.gameObject.CompareTag(Constants.Tag.Totem))
+            {
+                SendGameOverEvent();
+
+                if (OnNexusDestroyed != null)
+                {
+                    OnNexusDestroyed.Invoke(OwnerTeam);
+                }
+            }
+        }
+
+        // PRIVATE
+
+        private void SendGameOverEvent()
+        {
+            GameOver gameOverEvent = GameOver.Create();
+            gameOverEvent.WinningTeam = OwnerTeam.OppositeTeam().ToString();
+            gameOverEvent.Send();
+        }
+    }
+}
