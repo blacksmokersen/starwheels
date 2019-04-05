a14821e343c20db97dd2ab3d8bcb6c55570c9af0
Wydman <baptiste.int@gmail.com>
2019-04-05 22:04:47 +0200

    500 : Wall Ability Kart Prefab

--- Assets/Prefabs/AbilitiesStandAloneItems/AbilityWall/WallAbility.cs
+++ Assets/Prefabs/AbilitiesStandAloneItems/AbilityWall/WallAbility.cs
@@ -1,18 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Common.PhysicsUtils;
+using Bolt;
 
-public class WallAbility : MonoBehaviour
+namespace Abilities
 {
-    // Start is called before the first frame update
-    void Start()
+    public class WallAbility : Ability, IControllable
     {
-        
-    }
+        [SerializeField] private bool _enabled = true;
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set { _enabled = value; }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Events")]
+        public UnityEvent OnWallPreview;
+        public UnityEvent OnWallSpawn;
+
+        [Header("Conditions")]
+        [SerializeField] private GroundCondition _groundCondition;
+
+        [Header("WallPrefab")]
+        [SerializeField] private GameObject _wallAbilityPrefab;
+
+
+        private Transform _previewPosition;
+
+        private void Update()
+        {
+            if (entity.isAttached && entity.isControllerOrOwner && gameObject.activeInHierarchy)
+            {
+                MapInputs();
+            }
+        }
+
+        public void MapInputs()
+        {
+            if (Enabled && Input.GetButtonDown(Constants.Input.UseAbility))
+            {
+
+            }
+            if (Enabled && Input.GetButtonUp(Constants.Input.UseAbility))
+            {
+                InstantiateWall();
+            }
+
+
+
+        }
+
+
+
+        private void JumpToTopOfThePrefab()
+        {
+
+        }
+
+        private void InstantiateWall()
+        {
+            if (entity.isOwner)
+            {
+                Vector3 instantiatePosition = _previewPosition.position;
+
+                var wallPrefab = BoltNetwork.Instantiate(_wallAbilityPrefab, instantiatePosition, Quaternion.identity);
+
+
+            }
+        }
     }
 }
