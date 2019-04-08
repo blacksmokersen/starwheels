738a770f31e4764175d78489f047d6bec9ef8e6b
Shashimee <simon.trebern@outlook.com>
2019-04-08 14:03:00 +0200

    develop : Added hovering values in Game folder
    Former-commit-id: c5a900455e432be491bf74a96e4c2d1f29e22be0

--- Assets/Scripts/Common/Physics/Hovering.cs
+++ Assets/Scripts/Common/Physics/Hovering.cs
@@ -6,7 +6,7 @@ namespace Common.PhysicsUtils
     public class Hovering : MonoBehaviour
     {
         [SerializeField] private string _groundLayerName;
-        [SerializeField] private float _hoveringHeight;
+        [SerializeField] private FloatVariable _hoveringHeight;
 
         private Rigidbody _rb;
 
@@ -23,11 +23,11 @@ namespace Common.PhysicsUtils
         private void CheckGrounded()
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, _hoveringHeight, 1 << LayerMask.NameToLayer(_groundLayerName)))
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, _hoveringHeight.Value, 1 << LayerMask.NameToLayer(_groundLayerName)))
             {
                 _rb.useGravity = false;
                 _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
-                transform.position = new Vector3(transform.position.x, hit.point.y + _hoveringHeight, transform.position.z);
+                transform.position = new Vector3(transform.position.x, hit.point.y + _hoveringHeight.Value, transform.position.z);
             }
             else
             {
