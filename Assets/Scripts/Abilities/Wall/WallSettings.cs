f9e9904885c80c7c445f5e9fdbfc1a505898ba37
Wydman <baptiste.int@gmail.com>
2019-04-08 02:27:40 +0200

    500 : Kart WallAbility and Logic
    Former-commit-id: 3ad67f3db0c2538547f574f2545aa282df0ca7da

---
+++ Assets/Scripts/Abilities/Wall/WallSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Abilities
+{
+    [CreateAssetMenu(menuName = "Abilities Settings/Wall")]
+    public class WallSettings : AbilitySettings
+    {
+        [Header("Wall Duration")]
+        public float WallDuration;
+
+    }
+}
