ff8dcc00ec7a9b52b96996544fd1b708cd387654
Wydman <baptiste.int@gmail.com>
2019-04-11 18:02:56 +0200

    develop [Clean] : CloakPortal Removed and some Warnings Cleaning
    Former-commit-id: eb274d57d786b99903be17811316540282050720

--- Assets/Scripts/MapSpecifics/FindTotemForCam.cs
+++ Assets/Scripts/MapSpecifics/FindTotemForCam.cs
@@ -9,20 +9,15 @@ public class FindTotemForCam : MonoBehaviour
 
     private CinemachineVirtualCamera _cinemachine;
     private CinemachineTransposer _transposer;
-    private CinemachineComposer _composer;
 
     private BoltEntity _totem;
 
     private bool _totemFound = false;
 
- //   private CinemachineCollider _collider;
-
     private void Awake()
     {
         _cinemachine = GetComponent<CinemachineVirtualCamera>();
         _transposer = _cinemachine.GetCinemachineComponent<CinemachineTransposer>();
-        _composer = _cinemachine.GetCinemachineComponent<CinemachineComposer>();
-       // _collider = GetComponent<CinemachineCollider>();
     }
 
     private void Start()
