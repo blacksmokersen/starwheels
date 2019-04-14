7a4b23d898f41aa25ce8a06aba796667e6bd6be4
Shashimee <simon.trebern@outlook.com>
2019-04-14 14:26:33 +0200

    develop : Mine reworked
    Former-commit-id: 0c6d1c076ec88efbc41e41277cd14aff8e42a695

--- Assets/Scripts/Common/Physics/Hovering.cs
+++ Assets/Scripts/Common/Physics/Hovering.cs
@@ -9,6 +9,9 @@ namespace Common.PhysicsUtils
         [SerializeField] private FloatVariable _hoveringHeight;
 
         private Rigidbody _rb;
+        private bool _enabled = true;
+
+        // CORE
 
         private void Awake()
         {
@@ -20,18 +23,37 @@ namespace Common.PhysicsUtils
             CheckGrounded();
         }
 
+        // PUBLIC
+
+        public void Enable()
+        {
+            _rb.useGravity = true;
+            _enabled = true;
+        }
+
+        public void Disable()
+        {
+            _rb.useGravity = true;
+            _enabled = false;
+        }
+
+        // PRIVATE
+
         private void CheckGrounded()
         {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, _hoveringHeight.Value, 1 << LayerMask.NameToLayer(_groundLayerName)))
+            if (_enabled)
             {
-                _rb.useGravity = false;
-                _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
-                transform.position = new Vector3(transform.position.x, hit.point.y + _hoveringHeight.Value, transform.position.z);
-            }
-            else
-            {
-                _rb.useGravity = true;
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, Vector3.down, out hit, _hoveringHeight.Value, 1 << LayerMask.NameToLayer(_groundLayerName)))
+                {
+                    _rb.useGravity = false;
+                    _rb.velocity = new Vector3(_rb.velocity.x, 0, _rb.velocity.z);
+                    transform.position = new Vector3(transform.position.x, hit.point.y + _hoveringHeight.Value, transform.position.z);
+                }
+                else
+                {
+                    _rb.useGravity = true;
+                }
             }
         }
     }
