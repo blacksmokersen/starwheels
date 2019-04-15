2643ea484758444a42629d746f53b4f9f20aa968
Shashimee <simon.trebern@outlook.com>
2019-04-15 16:42:13 +0200

    513 : Basic handler for gamepad (WIP)
    Former-commit-id: ad819a14cb2e3a228034a57ed08fc3b62b46a63c

---
+++ Assets/Scripts/Common/MouseAndGamepadNavigation.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Common
+{
+    [DisallowMultipleComponent]
+    public class MouseAndGamepadNavigation : StandaloneInputModule
+    {
+        [SerializeField] private EventSystem _eventSystem;
+
+        private Selectable _lastSelected;
+        private bool _gamepadHasFocus = false;
+
+        // CORE
+
+        private new void Awake()
+        {
+            base.Awake();
+            _lastSelected = _eventSystem.firstSelectedGameObject.GetComponent<Selectable>();
+        }
+
+        private void Update()
+        {
+            CheckCurrentSelected();
+            CheckGamepadInputs();
+            CheckMouseInputs();
+        }
+
+        // PUBLIC
+
+        public GameObject GameObjectUnderPointer(int pointerId)
+        {
+            var lastPointer = GetLastPointerEventData(pointerId);
+            if (lastPointer != null)
+            {
+                return lastPointer.pointerCurrentRaycast.gameObject;
+            }
+            return null;
+        }
+
+        public GameObject GameObjectUnderPointer()
+        {
+            return GameObjectUnderPointer(PointerInputModule.kMouseLeftId);
+        }
+
+        // PRIVATE
+
+        private void CheckCurrentSelected()
+        {
+            if (_eventSystem.currentSelectedGameObject)
+            {
+                var currentSelected = _eventSystem.currentSelectedGameObject.GetComponent<Selectable>();
+                if (currentSelected != _lastSelected)
+                {
+                    _lastSelected = currentSelected;
+                }
+            }
+        }
+
+        private void CheckGamepadInputs()
+        {
+            if (!_gamepadHasFocus && (Mathf.Abs(Input.GetAxis(Constants.Input.TurnAxis)) > 0 || Mathf.Abs(Input.GetAxis(Constants.Input.UpAndDownAxis)) > 0))
+            {
+                //_lastSelected.Select();
+                _eventSystem.SetSelectedGameObject(_lastSelected.gameObject);
+                _gamepadHasFocus = true;
+                Cursor.visible = false;
+
+                if (_eventSystem.IsPointerOverGameObject())
+                {
+                    //GameObjectUnderPointer().GetComponent<Selectable>().
+                }
+            }
+        }
+
+        private void CheckMouseInputs()
+        {
+            if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0 || Mathf.Abs(Input.GetAxis("Mouse Y")) > 0)
+            {
+                Cursor.visible = true;
+                _gamepadHasFocus = false;
+            }
+        }
+    }
+}
