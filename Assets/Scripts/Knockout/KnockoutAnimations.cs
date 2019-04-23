aca71fa88302493819b2df3368514cc10499652b
Wydman <baptiste.int@gmail.com>
2019-04-23 17:43:52 +0200

    develop : Char Animations pt1
    Former-commit-id: 395418810aa36ca73440e9172c09c8eb61a8c5e2

--- Assets/Scripts/Knockout/KnockoutAnimations.cs
+++ Assets/Scripts/Knockout/KnockoutAnimations.cs
@@ -6,12 +6,15 @@ namespace Knockout
     {
         [Header("Animations")]
         [SerializeField] private Animator _animator;
+        [SerializeField] private Animator _charAnimator;
 
         public void PlayKnockoutAnimation()
         {
             Debug.Log("Playing anim");
             _animator.SetTrigger(Constants.Animations.Knockout);
             _animator.SetTrigger(("HitBack"));
+            _charAnimator.SetTrigger(Constants.Animations.Knockout);
+            _charAnimator.SetTrigger(("HitBack"));
         }
     }
 }
