6373414d852337eee0a4eb9fdf4e2ae3f33d650c
Shashimee <simon.trebern@outlook.com>
2019-04-23 14:10:28 +0200

    develop : Added KartDisabler for menus
    Former-commit-id: 8ca41e607921bf3656692400215818e29e61940b

---
+++ Assets/Scripts/Controls/KartDisabler.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using Common;
+
+namespace Controls
+{
+    public class KartDisabler : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private bool _disableKartOnEnabled;
+
+        private ControllableDisabler _disabler;
+
+        // CORE
+
+        private void OnEnable()
+        {
+            if (_disableKartOnEnabled)
+            {
+                Disable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_disableKartOnEnabled)
+            {
+                Enable();
+            }
+        }
+
+        // PUBLIC
+
+        public void Enable()
+        {
+            if (_disabler == null)
+            {
+                FindDisabler();
+            }
+            if (_disabler)
+            {
+                _disabler.EnableAllInChildren();
+            }
+        }
+
+        public void Disable()
+        {
+            if (_disabler == null)
+            {
+                FindDisabler();
+            }
+            if (_disabler)
+            {
+                _disabler.DisableAllInChildren();
+            }
+        }
+
+        // PRIVATE
+
+        private void FindDisabler()
+        {
+            foreach (var disabler in FindObjectsOfType<ControllableDisabler>())
+            {
+                if (disabler.CompareTag(Constants.Tag.Kart))
+                {
+                    _disabler = disabler;
+                }
+            }
+        }
+    }
+}
