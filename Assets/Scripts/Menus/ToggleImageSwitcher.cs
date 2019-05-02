b86a535d1fe04ee71f03a357b61358d2a3c645aa
Shashimee <simon.trebern@outlook.com>
2019-05-02 14:59:16 +0200

    520 : Continued menu

---
+++ Assets/Scripts/Menus/ToggleImageSwitcher.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menu
+{
+    [DisallowMultipleComponent]
+    public class ToggleImageSwitcher : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private Toggle _targetToggle;
+        [Space(15)]
+        [SerializeField] private Image _unselectedImage;
+        [SerializeField] private Image _onHoverImage;
+        [SerializeField] private Image _onHoverValidationInputImage;
+        [SerializeField] private Image _selectedImage;
+
+        // CORE
+
+        private void Awake()
+        {
+            SetUnselectedImage();
+        }
+
+        // PUBLIC
+
+        public void SetUnselectedImage()
+        {
+            _unselectedImage.gameObject.SetActive(true);
+            _onHoverImage.gameObject.SetActive(false);
+            _onHoverValidationInputImage.gameObject.SetActive(false);
+            _selectedImage.gameObject.SetActive(false);
+        }
+
+        public void SetHoverImage()
+        {
+            _unselectedImage.gameObject.SetActive(false);
+            _onHoverImage.gameObject.SetActive(true);
+            _onHoverValidationInputImage.gameObject.SetActive(true);
+            _selectedImage.gameObject.SetActive(false);
+        }
+
+        public void SetSelectedImage()
+        {
+            _unselectedImage.gameObject.SetActive(false);
+            _onHoverImage.gameObject.SetActive(false);
+            _onHoverValidationInputImage.gameObject.SetActive(false);
+            _selectedImage.gameObject.SetActive(true);
+        }
+    }
+}
