b0c7bdbb3e5b1d731d014279e176b9f0dbb8ad6d
Wydman <baptiste.int@gmail.com>
2019-06-17 18:15:32 +0200

    dev : Layered Audio Prefab

---
+++ Assets/Scripts/Common/Audio/LayeredAudioPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayeredAudioPlayer : MonoBehaviour
+{
+    [Header("AudioSources")]
+    [SerializeField] private AudioSource _baseLayerAudioSource;
+    [SerializeField] private AudioSource[] _LayersAudioSource;
+
+    private bool _searchingforBaseLayerToEnd = false;
+
+    private void Awake()
+    {
+        _baseLayerAudioSource.Play();
+    }
+
+    private void Update()
+    {
+        if (_searchingforBaseLayerToEnd)
+        {
+
+        }
+    }
+
+
+
+    public void PlayNextLayerWhenBaseLayerEnd()
+    {
+
+    }
+
+}
