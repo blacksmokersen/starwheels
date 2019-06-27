dbea0560d91704775290e0a195112502b4157093
Wydman <baptiste.int@gmail.com>
2019-06-27 17:30:03 +0200

    dev : Front wheels movements

--- Assets/Scripts/Engine/WheelsMeshMovement.cs
+++ Assets/Scripts/Engine/WheelsMeshMovement.cs
@@ -12,19 +12,36 @@ namespace Animations
 
 
         private float _wheelsSpeed;
+        private bool _resetFrontWheelsPosition = false;
 
         // PUBLIC
 
         public void FrontWheelsTurn(float currentAngle)
         {
-            var angleLeft = FrontWheelLeft.transform.localEulerAngles;
-            FrontWheelLeft.transform.localEulerAngles = new Vector3(angleLeft.x, currentAngle * 30, 0);
-            var angleRight = FrontWheelRight.transform.localEulerAngles;
-            FrontWheelRight.transform.localEulerAngles = new Vector3(angleRight.x, currentAngle * 30, 0);
+
+            if (Mathf.Abs(Input.GetAxis(Constants.Input.TurnAxis)) > 0.05f)
+            {
+                var angleLeft = FrontWheelLeft.transform.localEulerAngles;
+                FrontWheelLeft.transform.localEulerAngles = new Vector3(angleLeft.x, currentAngle * 30, 0);
+                var angleRight = FrontWheelRight.transform.localEulerAngles;
+                FrontWheelRight.transform.localEulerAngles = new Vector3(angleRight.x, currentAngle * 30, 0);
+                _resetFrontWheelsPosition = true;
+            }
+            else
+            {
+                if (_resetFrontWheelsPosition)
+                {
+                    var angleLeft = FrontWheelLeft.transform.localEulerAngles;
+                    FrontWheelLeft.transform.localEulerAngles = new Vector3(angleLeft.x, 0, 0);
+                    var angleRight = FrontWheelRight.transform.localEulerAngles;
+                    FrontWheelRight.transform.localEulerAngles = new Vector3(angleRight.x, 0, 0);
+                    _resetFrontWheelsPosition = false;
+                }
+            }
         }
 
         public void FrontWheelsRotation(float playerVelocity)
-        {            
+        {
             if (Mathf.Abs(playerVelocity) > 0.01f)
             {
                 FrontWheelLeft.transform.Rotate(Vector3.right * playerVelocity);
