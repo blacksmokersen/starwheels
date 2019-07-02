3bdb8b9d4d907e58f843be7912397061d43f2bf6
Wydman <baptiste.int@gmail.com>
2019-07-02 17:22:37 +0200

    dev : No HUD on intro cinematics

--- Assets/Scripts/Menus/CanvasDisabler.cs
+++ Assets/Scripts/Menus/CanvasDisabler.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
-public class CanvasDisabler : MonoBehaviour
+public class CanvasDisabler : GlobalEventListener
 {
     public bool IsDisabled = false;
+    [SerializeField] private CountdownSettings _countdownSettings;
+
+    //CORE
+
+    private void Start()
+    {
+        StartCoroutine(ForcePlayerHUD());
+
+        if (BoltNetwork.IsConnected)
+        {
+            if (!_countdownSettings.Countdown)
+                SwitchDisabler();
+        }
+        else
+            SwitchDisabler();
+    }
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.B))
         {
             IsDisabled = !IsDisabled;
             SwitchDisabler();
         }
     }
+
+    //BOLT
+
+    public override void OnEvent(LobbyCountdown evnt)
+    {
+        if (evnt.Time == 3)
+        {
+            SwitchDisabler();
+        }
+    }
+
+    //PUBLIC
 
     public void SwitchDisabler()
     {
@@ -20,5 +50,11 @@ public class CanvasDisabler : MonoBehaviour
             GetComponent<Canvas>().enabled = false;
         else
             GetComponent<Canvas>().enabled = true;
+    }
+
+    private IEnumerator ForcePlayerHUD()
+    {
+        yield return new WaitForSeconds(15);
+        GetComponent<Canvas>().enabled = true;
     }
 }
