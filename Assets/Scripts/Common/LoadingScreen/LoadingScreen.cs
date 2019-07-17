bdde0a508c723ab371fdcb6a40674ceb9679e590
Wydman <baptiste.int@gmail.com>
2019-07-17 15:19:19 +0200

    sw-develop : Loading Logic

--- Assets/Scripts/Common/LoadingScreen/LoadingScreen.cs
+++ Assets/Scripts/Common/LoadingScreen/LoadingScreen.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Bolt;
+using SW.Matchmaking;
+using Multiplayer;
 
 public class LoadingScreen : GlobalEventListener
 {
 
+
     [HideInInspector] public string SceneToLoad;
 
+    [SerializeField] private LobbyData _lobbyData;
 
     private bool _playersAreReady = false;
 
@@ -16,6 +20,11 @@ public class LoadingScreen : GlobalEventListener
 
 
     //CORE
+
+    private void Start()
+    {
+        StartLoadingGameScene(_lobbyData.ChosenMapName);
+    }
 
     private void Update()
     {
@@ -36,45 +45,54 @@ public class LoadingScreen : GlobalEventListener
 
     //PUBLIC
 
-    /*
+
     public void StartLoadingGameScene(string sceneName)
     {
         SceneToLoad = sceneName;
         StartCoroutine(LoadGameScene());
     }
-    */
+
 
     //PRIVATE
 
-    /*
-private IEnumerator LoadGameScene()
-{
-    Debug.Log("Starting MainMenu load.");
 
-    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneToLoad, LoadSceneMode.Single);
-    asyncLoad.allowSceneActivation = false;
-
-    while (!asyncLoad.isDone)
+    private IEnumerator LoadGameScene()
     {
-        yield return null;
-    }
+        Debug.Log("Starting MainMenu load.");
 
-    if (asyncLoad.isDone && _playersAreReady)
-    {
-        asyncLoad.allowSceneActivation = true;
-    }
-    */
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneToLoad, LoadSceneMode.Additive);
+        asyncLoad.allowSceneActivation = false;
 
-    /*
-    while (!asyncLoad.isDone)
-    {
-        if (_loadMainMenu)
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        if (BoltNetwork.IsServer)
+        {
+            var roomToken = new RoomProtocolToken() { Gamemode = _lobbyData.ChosenGamemode };
+            BoltNetwork.LoadScene(_lobbyData.ChosenMapName, roomToken);
+        }
+
+
+        /*
+        if (asyncLoad.isDone && _playersAreReady)
         {
             asyncLoad.allowSceneActivation = true;
         }
-        yield return null;
+        */
+
+        /*
+        while (!asyncLoad.isDone)
+        {
+            if (_loadMainMenu)
+            {
+                asyncLoad.allowSceneActivation = true;
+            }
+            yield return null;
+            */
     }
-    */
 }
+
 
 
