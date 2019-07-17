0cb3579ec616529f2c12b7314a7ff1aaea268bf4
Wydman <baptiste.int@gmail.com>
2019-07-17 14:11:18 +0200

    sw-develop : LoadingScreen scene, assets  and scripts

---
+++ Assets/Scripts/Common/LoadingScreen/LoadingScreen.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Bolt;
+
+public class LoadingScreen : GlobalEventListener
+{
+
+    [HideInInspector] public string SceneToLoad;
+
+
+    private bool _playersAreReady = false;
+
+
+
+
+    //CORE
+
+    private void Update()
+    {
+        if (_playersAreReady)
+        {
+
+        }
+    }
+
+
+    //BOLT
+
+    public override void OnEvent(PlayerReady evnt)
+    {
+        _playersAreReady = true;
+    }
+
+
+    //PUBLIC
+
+    /*
+    public void StartLoadingGameScene(string sceneName)
+    {
+        SceneToLoad = sceneName;
+        StartCoroutine(LoadGameScene());
+    }
+    */
+
+    //PRIVATE
+
+    /*
+private IEnumerator LoadGameScene()
+{
+    Debug.Log("Starting MainMenu load.");
+
+    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneToLoad, LoadSceneMode.Single);
+    asyncLoad.allowSceneActivation = false;
+
+    while (!asyncLoad.isDone)
+    {
+        yield return null;
+    }
+
+    if (asyncLoad.isDone && _playersAreReady)
+    {
+        asyncLoad.allowSceneActivation = true;
+    }
+    */
+
+    /*
+    while (!asyncLoad.isDone)
+    {
+        if (_loadMainMenu)
+        {
+            asyncLoad.allowSceneActivation = true;
+        }
+        yield return null;
+    }
+    */
+}
+
+
