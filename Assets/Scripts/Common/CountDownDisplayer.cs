71628c11f4d3e7337235fa3b950e8ea5103ebaa0
Wydman <baptiste.int@gmail.com>
2019-07-22 16:17:29 +0200

    663 : Added Displayed Coutdown

---
+++ Assets/Scripts/Common/CountDownDisplayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Bolt;
+
+public class CountDownDisplayer : GlobalEventListener
+{
+    [SerializeField] private TextMeshProUGUI _CountdownText;
+
+    public override void OnEvent(LobbyCountdown evnt)
+    {
+        if (evnt.Time >= 1 && evnt.Time < 6)
+        {
+            _CountdownText.gameObject.SetActive(true);
+            _CountdownText.text = " GAME START IN :  " + evnt.Time;
+        }
+        else if (evnt.Time == 0)
+        {
+            _CountdownText.text = " GAME STARTED !";
+            StartCoroutine(GameStartTimerDisplay(2));
+        }
+    }
+
+    private IEnumerator GameStartTimerDisplay(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        _CountdownText.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Common/LoadingScreen/LoadingScreen.cs
+++ Assets/Scripts/Common/LoadingScreen/LoadingScreen.cs
@@ -1,24 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using Bolt;
-using SW.Matchmaking;
-using Multiplayer;
 
 public class LoadingScreen : GlobalEventListener
 {
-
-
-    [HideInInspector] public string SceneToLoad;
-
-    [SerializeField] private LobbyData _lobbyData;
     [SerializeField] private GameObject _inGameMenu;
 
     private bool _playersAreReady = false;
-
-
-
 
     //CORE
 
@@ -31,24 +18,6 @@ public class LoadingScreen : GlobalEventListener
         }
     }
 
-    private void Start()
-    {
-      //  SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
-      //  StartLoadingGameScene(_lobbyData.ChosenMapName);
-    }
-
-    /*
-    private void Update()
-    {
-        if (_playersAreReady)
-        {
-            SceneManager.UnloadSceneAsync("LoadingScene");
-            _playersAreReady = false;
-           // StartGame();
-        }
-    }
-    */
-
     //BOLT
 
     public override void OnEvent(OnAllPlayersInGame evnt)
@@ -56,93 +25,6 @@ public class LoadingScreen : GlobalEventListener
         Debug.LogError("OnAllPlayersInGame INVOKE");
         _inGameMenu.SetActive(true);
         gameObject.SetActive(false);
-
-       // SceneManager.UnloadSceneAsync("LoadingScene");
-        // _playersAreReady = true;
-    }
-
-
-    //PUBLIC
-
-
-    public void StartLoadingGameScene(string sceneName)
-    {
-       // SceneToLoad = sceneName;
-      //  StartCoroutine(StartGameIn(3));
-      //  StartCoroutine(LoadGameScene());
-    }
-
-
-    //PRIVATE
-
-    private void StartGame()
-    {
-        var roomToken = new RoomProtocolToken() { Gamemode = _lobbyData.ChosenGamemode, PlayersCount = _lobbyData.CurrentPlayers};
-        BoltNetwork.LoadScene(_lobbyData.ChosenMapName, roomToken);
-    }
-
-
-        private IEnumerator StartGameIn(float timer)
-    {
-        yield return new WaitForSeconds(timer);
-
-        var roomToken = new RoomProtocolToken() { Gamemode = _lobbyData.ChosenGamemode, PlayersCount = _lobbyData.CurrentPlayers };
-
-        BoltNetwork.LoadScene(_lobbyData.ChosenMapName, roomToken);
-
-    }
-
-    private IEnumerator LoadGameScene()
-    {
-        Debug.Log("Starting MainMenu load.");
-
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneToLoad, LoadSceneMode.Additive);
-        asyncLoad.allowSceneActivation = false;
-
-        while (!asyncLoad.isDone)
-        {
-            Debug.Log("1");
-            if (asyncLoad.progress >= 0.9f)
-            {
-              //  asyncLoad.allowSceneActivation = true;
-                if (BoltNetwork.IsServer)
-                {
-                    Debug.Log("2");
-                    _playersAreReady = true;
-                    /*
-                    var roomToken = new RoomProtocolToken() { Gamemode = _lobbyData.ChosenGamemode };
-                    BoltNetwork.LoadScene(_lobbyData.ChosenMapName, roomToken);
-                    */
-                }
-                Debug.Log("3");
-            }
-            yield return null;
-        }
-        Debug.Log("4");
-        /*
-        if (BoltNetwork.IsServer)
-        {
-            var roomToken = new RoomProtocolToken() { Gamemode = _lobbyData.ChosenGamemode };
-            BoltNetwork.LoadScene(_lobbyData.ChosenMapName, roomToken);
-        }
-        */
-
-        /*
-        if (asyncLoad.isDone && _playersAreReady)
-        {
-            asyncLoad.allowSceneActivation = true;
-        }
-        */
-
-        /*
-        while (!asyncLoad.isDone)
-        {
-            if (_loadMainMenu)
-            {
-                asyncLoad.allowSceneActivation = true;
-            }
-            yield return null;
-            */
     }
 }
 
