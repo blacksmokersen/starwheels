1562a66dea994604bd43a907c5b3e4efbde568da
Wydman <baptiste.int@gmail.com>
2019-07-24 17:58:29 +0200

    665 : End game Display

---
+++ Assets/Scripts/Common/EndGameDisplayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using CameraUtils;
+
+namespace Gamemodes
+{
+    public class EndGameDisplayer : GlobalEventListener
+    {
+        [SerializeField] private GameObject _display;
+        [SerializeField] private GameObject _displayCamera;
+
+        public override void OnEvent(GameOver evnt)
+        {
+            _display.SetActive(true);
+            _displayCamera.SetActive(true);
+
+            if (BoltNetwork.IsServer)
+            {
+                foreach (GameObject kart in SWExtensions.KartExtensions.GetKartsWithTeam((Team)evnt.WinningTeam))
+                {
+                    kart.gameObject.transform.position = new Vector3(_display.transform.position.x, _display.transform.position.y + 2, _display.transform.position.z);
+                }
+            }
+        }
+    }
+}
