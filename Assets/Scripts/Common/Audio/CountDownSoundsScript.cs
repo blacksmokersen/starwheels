0dbe3fb53c0656682b6632d504ae6ef26af1a2a4
Wydman <baptiste.int@gmail.com>
2019-08-02 18:25:10 +0200

    670 : Added countdown Sounds

--- Assets/Scripts/Common/Audio/CountDownSoundsScript.cs
+++ Assets/Scripts/Common/Audio/CountDownSoundsScript.cs
@@ -1,32 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Bolt;
 
 public class CountDownSoundsScript : GlobalEventListener
 {
-
-    [SerializeField] private AudioSource _numberCooldown;
-    [SerializeField] private AudioSource _areYouReady;
-
-    private bool _alreadyLaunched = false;
-
-
-    public override void OnEvent(OnAllPlayersInGame evnt)
-    {
-        if (evnt.IsGameAlreadyStarted)
-        {
-            _numberCooldown.Play();
-            _alreadyLaunched = true;
-        }
-    }
+    [SerializeField] private AudioSource _mapIntroSound;
+    [SerializeField] private AudioSource _countdown;
 
     public override void OnEvent(LobbyCountdown evnt)
     {
-        if (!_alreadyLaunched)
-        {
-            _numberCooldown.Play();
-            _alreadyLaunched = true;
-        }
+        if (evnt.Time == 7)
+            _mapIntroSound.Play();
+
+        if (evnt.Time == 3)
+            _countdown.Play();
     }
 }
