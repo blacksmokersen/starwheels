d9754792a9832d40348a0314b328c083af8d56cc
Louis G <gilbert.louis27@gmail.com>
2019-08-09 19:24:14 +0200

    673-Menu corrections and Customization integration

---
+++ Assets/Scripts/Menus/StandardsButtons/ButtonToggleEvents.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Menu.Options
+{
+    public class ButtonToggleEvents : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent  _falseEvent;
+        [SerializeField] private UnityEvent  _trueEvent;
+
+        public void ToggleEvent(bool value)
+        {
+            if (value)
+            {
+                if (_trueEvent != null)
+                {
+                    _trueEvent.Invoke();
+                }
+            }
+            else
+            {
+                if (_falseEvent != null)
+                {
+                    _falseEvent.Invoke();
+                }
+            }
+        }
+    }
+}
