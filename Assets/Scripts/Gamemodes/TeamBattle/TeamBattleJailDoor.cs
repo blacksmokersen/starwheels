931d68a19b9daa16a0a569661051ecc06d77b44c
Wydman <baptiste.int@gmail.com>
2019-08-16 01:01:28 +0200

    674 : JailDoor and JailButton support, new jail bolts events

--- Assets/Scripts/Gamemodes/TeamBattle/TeamBattleJailDoor.cs
+++ Assets/Scripts/Gamemodes/TeamBattle/TeamBattleJailDoor.cs
@@ -2,8 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Multiplayer;
+using Bolt;
 
-public class TeamBattleJailDoor : MonoBehaviour
+public class TeamBattleJailDoor : GlobalEventListener
 {
     [SerializeField] private Team _doorTeam;
+    private Animator _doorAnimator;
+    private Coroutine _doorAnimatorCoroutine;
+
+    //CORE
+
+    private void Awake()
+    {
+        _doorAnimator = GetComponent<Animator>();
+    }
+
+    //BOLT
+
+    public override void OnEvent(JailButtonPushed evnt)
+    {
+        if (evnt.Team == _doorTeam.ToString())
+        {
+            _doorAnimatorCoroutine = StartCoroutine(OpenDoor());
+        }
+    }
+
+    //PRIVATE
+
+    private IEnumerator OpenDoor()
+    {
+        _doorAnimator.SetTrigger("OpenDoor");
+        yield return new WaitForSeconds(5);
+        _doorAnimator.SetTrigger("CloseDoor");
+    }
 }
--- Assets/Scripts/Gamemodes/TeamBattle/teamBattleJailButton.cs
+++ Assets/Scripts/Gamemodes/TeamBattle/teamBattleJailButton.cs
@@ -2,16 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Multiplayer;
+using Bolt;
 
-public class teamBattleJailButton : MonoBehaviour
+public class TeamBattleJailButton : GlobalEventListener
 {
     [SerializeField] private Team _jailButtonTeam;
+    [SerializeField] private float _jailButtonCooldown;
 
-    private void OnTriggerEnter(Collider player)
+    private bool _isJailButtonEnabled = true;
+
+    //CORE
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (player.GetComponent<PlayerInfo>().Team != _jailButtonTeam)
+        //  if (entity.isAttached && entity.isOwner && other.CompareTag(Constants.Tag.KartCollider))
+        //  {
+        if (_isJailButtonEnabled)
         {
-            // lancer bolt event
+            if (other.CompareTag(Constants.Tag.KartCollider))
+            {
+                if (other.GetComponentInParent<PlayerInfo>().Team != _jailButtonTeam)
+                {
+                    JailButtonPushed jailButtonPushed = JailButtonPushed.Create();
+                    jailButtonPushed.PlayerID = other.GetComponentInParent<PlayerInfo>().OwnerID;
+                    jailButtonPushed.Team = _jailButtonTeam.ToString();
+                    jailButtonPushed.Send();
+                }
+                else if (other.GetComponentInParent<PlayerInfo>().Team == _jailButtonTeam)
+                {
+
+                }
+            }
         }
+    }
+
+    //BOLT
+
+    public override void OnEvent(JailButtonPushed evnt)
+    {
+        if (evnt.Team == _jailButtonTeam.ToString())
+        {
+            StartCoroutine(JailButtonCooldown());
+        }
+    }
+
+    //PRIVATE
+
+    private IEnumerator JailButtonCooldown()
+    {
+        _isJailButtonEnabled = false;
+        yield return new WaitForSeconds(_jailButtonCooldown);
+        _isJailButtonEnabled = true;
     }
 }
