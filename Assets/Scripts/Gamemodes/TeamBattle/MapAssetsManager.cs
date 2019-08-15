61b98fb1902891dfdda95bce9c4e28bb90bbd601
Wydman <baptiste.int@gmail.com>
2019-08-15 20:39:58 +0200

    674 : Link base map assets

--- Assets/Scripts/Gamemodes/TeamBattle/MapAssetsManager.cs
+++ Assets/Scripts/Gamemodes/TeamBattle/MapAssetsManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class MapAssetsManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _redJailDoor;
+    [SerializeField] private GameObject _blueJailDoor;
+
+    [SerializeField] private GameObject _redButton;
+    [SerializeField] private GameObject _blueButton;
+
     [SerializeField] private GameObject[] _TeamBattleAssetsToEnable;
     [SerializeField] private GameObject[] _BaseMapAssetsToDisable;
 
