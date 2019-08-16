b66c6ea012cdeaa18a3a7ccd0dcf6cb5a98be29a
Louis G <gilbert.louis27@gmail.com>
2019-08-16 17:13:28 +0200

    675 : Sound options add

--- Assets/Scripts/Menus/Options/AudioPanelSettings.cs
+++ Assets/Scripts/Menus/Options/AudioPanelSettings.cs
@@ -11,11 +11,19 @@ namespace Menu.Options
         [SerializeField] private AudioMixer _audioMixer;
 
         [Header("UI Elements")]
+        [SerializeField] private Slider _masterSlider;
         [SerializeField] private Slider _sfxSlider;
         [SerializeField] private Slider _musicSlider;
+        [SerializeField] private Slider _menuSoundsSlider;
+        [SerializeField] private Slider _voiceSlider;
+        [SerializeField] private Slider _advertSlider;
 
+        private const string _masterMixerName = "Master";
         private const string _masterSFXMixerName = "MasterSFX";
         private const string _masterMusicMixerName = "MasterMusic";
+        private const string _masterMenuSoundsMixerName = "MenuSound";
+        private const string _masterVoiceLinesMixerName = "VoiceLines";
+        private const string _masterAdvertMixerName = "VoiceLinesGameModes";
 
         // CORE
 
@@ -31,6 +39,12 @@ namespace Menu.Options
 
         // PUBLIC
 
+        public void SetMasterVolume(float value)
+        {
+            _audioMixer.SetFloat(_masterMixerName, value);
+            PlayerPrefs.SetFloat(_masterMixerName, value);
+        }
+
         public void SetSFXVolume(float value)
         {
             _audioMixer.SetFloat(_masterSFXMixerName, value);
@@ -43,12 +57,34 @@ namespace Menu.Options
             PlayerPrefs.SetFloat(_masterMusicMixerName, value);
         }
 
+        public void SetHUDVolume(float value)
+        {
+            _audioMixer.SetFloat(_masterMenuSoundsMixerName, value);
+            PlayerPrefs.SetFloat(_masterMenuSoundsMixerName, value);
+        }
+
+        public void SetVoicesVolume(float value)
+        {
+            _audioMixer.SetFloat(_masterVoiceLinesMixerName, value);
+            PlayerPrefs.SetFloat(_masterVoiceLinesMixerName, value);
+        }
+
+        public void SetAdvertVolume(float value)
+        {
+            _audioMixer.SetFloat(_masterAdvertMixerName, value);
+            PlayerPrefs.SetFloat(_masterAdvertMixerName, value);
+        }
+
         // PRIVATE
 
         private void InitializeUI()
         {
+            _masterSlider.value = PlayerPrefs.GetFloat(_masterMixerName);
             _sfxSlider.value = PlayerPrefs.GetFloat(_masterSFXMixerName);
             _musicSlider.value = PlayerPrefs.GetFloat(_masterMusicMixerName);
+            _menuSoundsSlider.value = PlayerPrefs.GetFloat(_masterMenuSoundsMixerName);
+            _voiceSlider.value = PlayerPrefs.GetFloat(_masterVoiceLinesMixerName);
+            _advertSlider.value = PlayerPrefs.GetFloat(_masterAdvertMixerName);
         }
     }
 }
