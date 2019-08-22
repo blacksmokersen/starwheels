18fd949a627423e2e36fa2e46d4e665d441cbf79
Wydman <baptiste.int@gmail.com>
2019-08-22 22:18:08 +0200

    sw-develop : Fix players nickname and count on TinyLobby

--- Assets/Scripts/Matchmaking/Lobby/LobbyPlayerInfoUpdater.cs
+++ Assets/Scripts/Matchmaking/Lobby/LobbyPlayerInfoUpdater.cs
@@ -24,67 +24,51 @@ namespace SW.Matchmaking
         [SerializeField] private TextMeshProUGUI _currentPlayerCountText;
 
         private List<LobbyPlayerInfoEntry> _entries = new List<LobbyPlayerInfoEntry>();
+        private List<string> _serverSidePlayerNicknameList = new List<string>();
         private bool _sessionCreated = false;
 
         // BOLT
-
         public override void SessionCreated(UdpSession session)
         {
             if (!_sessionCreated)
             {
-                CreatePlayerEntry(_myPlayerSettings.Nickname);
-                UpdateCurrentPlayerCount(1);
-
+                _serverSidePlayerNicknameList.Add(_myPlayerSettings.Nickname);
+                SendNicknameListToAllPlayers();
                 _sessionCreated = true;
             }
         }
 
-        public override void SessionConnected(UdpSession session, IProtocolToken token)
-        {
-            var lobbyToken = (LobbyToken)session.GetProtocolToken();
-            BuildPlayerList(lobbyToken.PlayersNicknames);
-        }
-
         public override void Connected(BoltConnection connection)
         {
-            var joinToken = (JoinToken) connection.ConnectToken;
-
             if (BoltNetwork.IsServer)
             {
-                var playerCount = 1 + SWMatchmaking.GetCurrentLobbyPlayerCount();
-                LobbyPlayerJoined lobbyPlayerJoinedEvent = LobbyPlayerJoined.Create();
-                lobbyPlayerJoinedEvent.LobbyPlayerCount = playerCount;
-                lobbyPlayerJoinedEvent.PlayerID = (int)connection.ConnectionId;
-                lobbyPlayerJoinedEvent.PlayerNickname = joinToken.Nickname;
-                lobbyPlayerJoinedEvent.Send();
+                var joinToken = (JoinToken)connection.ConnectToken;
+                _serverSidePlayerNicknameList.Add(joinToken.Nickname);
+                SendNicknameListToAllPlayers();
+            }
+        }
+
+        public override void Disconnected(BoltConnection connection)
+        {
+            if (BoltNetwork.IsServer)
+            {
+                var leaveToken = (JoinToken)connection.ConnectToken;
+                _serverSidePlayerNicknameList.Remove(leaveToken.Nickname);
+                SendNicknameListToAllPlayers();
             }
         }
 
         public override void OnEvent(LobbyPlayerJoined evnt)
         {
-            CreatePlayerEntry(evnt.PlayerNickname);
-            UpdateCurrentPlayerCount(evnt.LobbyPlayerCount);
-        }
-
-        public override void Disconnected(BoltConnection connection)
-        {
-            var joinToken = (JoinToken)connection.ConnectToken;
-
-            if (BoltNetwork.IsServer)
+            if (evnt.CleanEntries)
             {
-                var playerCount = SWMatchmaking.GetCurrentLobbyPlayerCount();
-                LobbyPlayerLeft lobbyPlayerLeftEvent = LobbyPlayerLeft.Create();
-                lobbyPlayerLeftEvent.LobbyPlayerCount = playerCount;
-                lobbyPlayerLeftEvent.PlayerID = (int)connection.ConnectionId;
-                lobbyPlayerLeftEvent.PlayerNickname = joinToken.Nickname;
-                lobbyPlayerLeftEvent.Send();
+                ResetList();
             }
-        }
-
-        public override void OnEvent(LobbyPlayerLeft evnt)
-        {
-            RemovePlayerEntry(evnt.PlayerNickname);
-            UpdateCurrentPlayerCount(evnt.LobbyPlayerCount);
+            else
+            {
+                CreatePlayerEntry(evnt.PlayerNickname);
+                UpdateCurrentPlayerCount(evnt.LobbyPlayerCount);
+            }
         }
 
         public override void BoltShutdownBegin(AddCallback registerDoneCallback)
@@ -92,17 +76,37 @@ namespace SW.Matchmaking
             CleanList();
         }
 
-        // PRIVATE
+        //PUBLIC
 
-        private void UpdateCurrentPlayerCount(int playerCount)
+        public void ResetServerList()
         {
-            _currentPlayerCountText.text = playerCount + " players";
-
             if (BoltNetwork.IsServer)
             {
-                _lobbyData.CurrentPlayers = playerCount;
-                SWMatchmaking.UpdateLobbyData(_lobbyData);
+                _serverSidePlayerNicknameList.Clear();
             }
+        }
+
+        //PRIVATE
+
+        private void SendNicknameListToAllPlayers()
+        {
+            SendResetListEvent();
+
+            var playerCount = 1 + SWMatchmaking.GetCurrentLobbyPlayerCount();
+            foreach (string nickname in _serverSidePlayerNicknameList)
+            {
+                LobbyPlayerJoined lobbyPlayerJoinedEvent = LobbyPlayerJoined.Create();
+                lobbyPlayerJoinedEvent.LobbyPlayerCount = playerCount;
+                lobbyPlayerJoinedEvent.PlayerNickname = nickname;
+                lobbyPlayerJoinedEvent.Send();
+            }
+        }
+
+        private void SendResetListEvent()
+        {
+            LobbyPlayerJoined lobbyPlayerJoinedEvent = LobbyPlayerJoined.Create();
+            lobbyPlayerJoinedEvent.CleanEntries = true;
+            lobbyPlayerJoinedEvent.Send();
         }
 
         private void CreatePlayerEntry(string nickname)
@@ -121,29 +125,6 @@ namespace SW.Matchmaking
             }
         }
 
-        private void BuildPlayerList(List<string> playerList)
-        {
-            foreach (var playerNickname in playerList)
-            {
-                CreatePlayerEntry(playerNickname);
-            }
-        }
-
-        private void RemovePlayerEntry(string nickname)
-        {
-            if (NicknameInList(nickname))
-            {
-                _lobbyData.PlayersNicknames.Remove(nickname);
-                var entry = FindEntryForNickname(nickname);
-                _entries.Remove(entry);
-                Destroy(entry.gameObject);
-            }
-            else
-            {
-                Debug.LogError("[LOBBY] No ID found for the player. Couldn't remove his entry.");
-            }
-        }
-
         private bool NicknameInList(string nickname)
         {
             foreach (var entry in _entries)
@@ -156,16 +137,24 @@ namespace SW.Matchmaking
             return false;
         }
 
-        private LobbyPlayerInfoEntry FindEntryForNickname(string nickname)
+        private void UpdateCurrentPlayerCount(int playerCount)
         {
-            foreach (var entry in _entries)
+            _currentPlayerCountText.text = playerCount + " players";
+
+            if (BoltNetwork.IsServer)
             {
-                if (entry.Nickname.Equals(nickname))
-                {
-                    return entry;
-                }
+                _lobbyData.CurrentPlayers = playerCount;
+                SWMatchmaking.UpdateLobbyData(_lobbyData);
             }
-            return null;
+        }
+
+        private void ResetList()
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+            _entries.Clear();
         }
 
         private void CleanList()
@@ -176,7 +165,164 @@ namespace SW.Matchmaking
             }
             _entries.Clear();
             _lobbyData.PlayersNicknames.Clear();
+            UpdateCurrentPlayerCount(0);
             _sessionCreated = false;
         }
+
+        #region OLD
+        /*
+    public override void SessionCreated(UdpSession session)
+    {
+        if (!_sessionCreated)
+        {
+            CreatePlayerEntry(_myPlayerSettings.Nickname);
+            UpdateCurrentPlayerCount(1);
+
+            _sessionCreated = true;
+        }
+    }
+
+    public override void SessionConnected(UdpSession session, IProtocolToken token)
+    {
+        var lobbyToken = (LobbyToken)session.GetProtocolToken();
+        BuildPlayerList(lobbyToken.PlayersNicknames);
+    }
+
+    public override void Connected(BoltConnection connection)
+    {
+        var joinToken = (JoinToken)connection.ConnectToken;
+
+        if (BoltNetwork.IsServer)
+        {
+            var playerCount = 1 + SWMatchmaking.GetCurrentLobbyPlayerCount();
+            LobbyPlayerJoined lobbyPlayerJoinedEvent = LobbyPlayerJoined.Create();
+            lobbyPlayerJoinedEvent.LobbyPlayerCount = playerCount;
+            lobbyPlayerJoinedEvent.PlayerID = (int)connection.ConnectionId;
+            lobbyPlayerJoinedEvent.PlayerNickname = joinToken.Nickname;
+            lobbyPlayerJoinedEvent.Send();
+        }
+    }
+
+    public override void OnEvent(LobbyPlayerJoined evnt)
+    {
+        CreatePlayerEntry(evnt.PlayerNickname);
+        UpdateCurrentPlayerCount(evnt.LobbyPlayerCount);
+    }
+
+    public override void Disconnected(BoltConnection connection)
+    {
+        var joinToken = (JoinToken)connection.ConnectToken;
+
+        if (BoltNetwork.IsServer)
+        {
+            var playerCount = SWMatchmaking.GetCurrentLobbyPlayerCount();
+            LobbyPlayerLeft lobbyPlayerLeftEvent = LobbyPlayerLeft.Create();
+            lobbyPlayerLeftEvent.LobbyPlayerCount = playerCount;
+            lobbyPlayerLeftEvent.PlayerID = (int)connection.ConnectionId;
+            lobbyPlayerLeftEvent.PlayerNickname = joinToken.Nickname;
+            lobbyPlayerLeftEvent.Send();
+        }
+    }
+
+    public override void OnEvent(LobbyPlayerLeft evnt)
+    {
+        RemovePlayerEntry(evnt.PlayerNickname);
+        UpdateCurrentPlayerCount(evnt.LobbyPlayerCount);
+    }
+
+    public override void BoltShutdownBegin(AddCallback registerDoneCallback)
+    {
+        CleanList();
+    }
+
+    // PRIVATE
+
+    private void UpdateCurrentPlayerCount(int playerCount)
+    {
+        _currentPlayerCountText.text = playerCount + " players";
+
+        if (BoltNetwork.IsServer)
+        {
+            _lobbyData.CurrentPlayers = playerCount;
+            SWMatchmaking.UpdateLobbyData(_lobbyData);
+        }
+    }
+
+    private void CreatePlayerEntry(string nickname)
+    {
+        if (!NicknameInList(nickname))
+        {
+            var entry = Instantiate(_nicknameEntryPrefab, transform, false).GetComponent<LobbyPlayerInfoEntry>();
+            entry.SetNickname(nickname);
+
+            _entries.Add(entry);
+            _lobbyData.PlayersNicknames.Add(nickname);
+        }
+        else
+        {
+            Debug.LogError("[LOBBY] ID already found for the player. Couldn't add another entry.");
+        }
+    }
+
+    private void BuildPlayerList(List<string> playerList)
+    {
+        foreach (var playerNickname in playerList)
+        {
+            CreatePlayerEntry(playerNickname);
+        }
+    }
+
+    private void RemovePlayerEntry(string nickname)
+    {
+        if (NicknameInList(nickname))
+        {
+            _lobbyData.PlayersNicknames.Remove(nickname);
+            var entry = FindEntryForNickname(nickname);
+            _entries.Remove(entry);
+            Destroy(entry.gameObject);
+        }
+        else
+        {
+            Debug.LogError("[LOBBY] No ID found for the player. Couldn't remove his entry.");
+        }
+    }
+
+    private bool NicknameInList(string nickname)
+    {
+        foreach (var entry in _entries)
+        {
+            if (entry.Nickname.Equals(nickname))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private LobbyPlayerInfoEntry FindEntryForNickname(string nickname)
+    {
+        foreach (var entry in _entries)
+        {
+            if (entry.Nickname.Equals(nickname))
+            {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    private void CleanList()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        _entries.Clear();
+        _lobbyData.PlayersNicknames.Clear();
+        _sessionCreated = false;
+    }
+    */
+
+        #endregion
     }
 }
