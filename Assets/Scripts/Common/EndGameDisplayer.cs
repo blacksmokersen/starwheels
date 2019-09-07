c943c7b3794710ae703a18abf96e4475a4f62540
Wydman <baptiste.int@gmail.com>
2019-09-07 13:47:37 +0200

    sw-develop : Team end game display support

--- Assets/Scripts/Common/EndGameDisplayer.cs
+++ Assets/Scripts/Common/EndGameDisplayer.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Bolt;
 using CameraUtils;
+using Multiplayer;
 
 namespace Gamemodes
 {
     public class EndGameDisplayer : GlobalEventListener
     {
-        [SerializeField] private GameObject _display;
         [SerializeField] private GameObject _displayCamera;
-        [SerializeField] private Transform _kartPosition;
+        [SerializeField] private List<GameObject> _displays = new List<GameObject>();
+        [SerializeField] private List<Transform> _standPositions = new List<Transform>();
+
+        // private Dictionary<string, Transform> _respawns = new Dictionary<string, Transform>();
 
         public override void OnEvent(GameOver evnt)
         {
-            _display.SetActive(true);
+            foreach (GameObject display in _displays)
+            {
+                display.SetActive(true);
+            }
             _displayCamera.SetActive(true);
 
+            var standNumber = 0;
             foreach (GameObject kart in SWExtensions.KartExtensions.GetKartsWithTeam((Team)evnt.WinningTeam))
             {
                 kart.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-                kart.gameObject.transform.position = _kartPosition.position;
+                kart.gameObject.transform.position = _standPositions[standNumber].position;
+                standNumber++;
             }
         }
     }
