175d4757ef1c491c629202e835af05dec25d4821
Louis G <gilbert.louis27@gmail.com>
2019-09-16 18:16:03 +0200

    678-FreeCam improved

--- Assets/Scripts/Camera/FreeCamera.cs
+++ Assets/Scripts/Camera/FreeCamera.cs
@@ -45,6 +45,18 @@ namespace CameraUtils
             {
                 transform.eulerAngles += new Vector3(0, Input.GetAxis(Constants.Input.TurnCamera) * _rotateXSpeed, 0);
                 transform.eulerAngles += new Vector3(-Input.GetAxis(Constants.Input.UpAndDownCamera) * _rotateYSpeed, 0, 0);
+                if (Input.GetAxis(Constants.Input.TurnCamera) == 0)
+                {
+                    if (Input.GetKeyDown(KeyCode.Q))
+                    {
+                        transform.eulerAngles += new Vector3(0, - _rotateXSpeed, 0);
+                    }
+                    if (Input.GetKeyDown(KeyCode.D))
+                    {
+                        transform.eulerAngles += new Vector3(0, _rotateXSpeed, 0);
+                    }
+                }
+
 
                 if (_switchInputs)
                 {
@@ -57,8 +69,47 @@ namespace CameraUtils
                 {
                     transform.Translate(Vector3.forward * Input.GetAxis(Constants.Input.UpAndDownAxis) * _forwardSpeed);
                     transform.Translate(Vector3.right * Input.GetAxis(Constants.Input.TurnAxis) * _horizontalSpeed);
-                    transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.Accelerate) * _verticalSpeed);
-                    transform.Translate(Vector3.down * Input.GetAxis(Constants.Input.Decelerate) * _verticalSpeed);
+                    //transform.Translate(Vector3.up * Input.GetAxis(Constants.Input.Accelerate) * _verticalSpeed);
+                    transform.Translate(Vector3.down * Input.GetAxis(Constants.Input.Triggers) * _verticalSpeed);
+
+                    if (Input.GetAxis(Constants.Input.UpAndDownAxis) == 0)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Z))
+                        {
+                            transform.Translate(Vector3.forward * _forwardSpeed);
+                        }
+                        if (Input.GetKeyDown(KeyCode.S))
+                        {
+                            transform.Translate(Vector3.back * _forwardSpeed);
+                        }
+                    }
+                    if (Input.GetAxis(Constants.Input.TurnAxis) == 0)
+                    {
+                        if (Input.GetKeyDown(KeyCode.A))
+                        {
+                            transform.Translate(Vector3.left * _forwardSpeed);
+                        }
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            transform.Translate(Vector3.right * _forwardSpeed);
+                        }
+                    }
+
+                    if (Input.GetAxis(Constants.Input.Accelerate) == 0)
+                    {
+                        if (Input.GetKeyDown(KeyCode.Space))
+                        {
+                            transform.Translate(Vector3.up * _verticalSpeed);
+                        }
+                    }
+
+                    if (Input.GetAxis(Constants.Input.Decelerate) == 0)
+                    {
+                        if (Input.GetKeyDown(KeyCode.LeftShift))
+                        {
+                            transform.Translate(Vector3.down * _verticalSpeed);
+                        }
+                    }
                 }
             }
         }
